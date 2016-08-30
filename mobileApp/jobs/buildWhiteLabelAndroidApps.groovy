d1c07f0b5db596ccca0c301d02425b66fbb35d61
stu <stucontact@gmail.com>
2016-08-30 15:42:29 -0400

    implement signing scripts in android build job

--- mobileApp/jobs/buildWhiteLabelAndroidApps.groovy
+++ mobileApp/jobs/buildWhiteLabelAndroidApps.groovy
@@ -8,10 +8,16 @@ Example secret YAML file used by this script
 jobConfig:
     jobName: build-demo-android-app
     public: true
+    release: false
+    buildScript: build.sh
+    gradleProperties: GRADLE_PROPERTIES
+    keystore: APP_KEYSTORE
+    keystoreFile: APP_KEYSTORE_FILE
     gitRepo: https://github.com/org/project.git
     gitCredential: git_user
     appBaseDir: android-app-project
     appBaseName: edx-demo-app
+    generatedApkName: edx-demo-app.apk
     sshAgent: ssh_user
     hockeyAppApiToken: abc123
 */
@@ -35,6 +41,7 @@ catch (any) {
     out.println('Exiting with error code 1')
     return 1
 }
+
 out.println('Successfully parsed secret YAML file')
 
 // Iterate over the configurations in BUILD_ANDROID_SECRET
@@ -45,10 +52,16 @@ secretMap.each { jobConfigs ->
     // Test parsed secret contains the correct fields
     assert jobConfig.containsKey('jobName')
     assert jobConfig.containsKey('public')
+    assert jobConfig.containsKey('release')
+    assert jobConfig.containsKey('buildScript')
+    assert jobConfig.containsKey('gradleProperties')
+    assert jobConfig.containsKey('keystore')
+    assert jobConfig.containsKey('keystoreFile')
     assert jobConfig.containsKey('gitRepo')
     assert jobConfig.containsKey('gitCredential')
     assert jobConfig.containsKey('appBaseDir')
     assert jobConfig.containsKey('appBaseName')
+    assert jobConfig.containsKey('generatedApkName')
     assert jobConfig.containsKey('sshAgent')
     assert jobConfig.containsKey('hockeyAppApiToken')
 
@@ -72,7 +85,9 @@ secretMap.each { jobConfigs ->
         // Run this build on the Jenkins worker configured with the Android SDK
         label('android-worker')
 
-        // Parameterize build with "HASH", the git hash used to pull source
+        // Parameterize build with:
+        //"HASH", the git hash used to pull the app source code
+        //"RELEASE_NOTES", a string (multi-line) for annotating the builds published to hockeyApp
         parameters {
             stringParam('HASH', 'refs/heads/master', 'Git hash of the project to build. Default = most recent hash of master.')
             textParam('RELEASE_NOTES', 'Test Build', 'Plain text release notes. Add content here and it will be published to HockeyApp along with the app.')
@@ -106,16 +121,32 @@ secretMap.each { jobConfigs ->
             sshAgent(jobConfig['sshAgent'])
         }
 
-        // Inject environment variables to make the shell script generic
+        // Inject environment variables to make the build script generic
         environmentVariables {
+            env('ANDROID_HOME', '/opt/Android/Sdk')
+            env('BUILD_SCRIPT', jobConfig['buildScript'])
             env('APP_BASE_DIR', jobConfig['appBaseDir'])
             env('APP_BASE_NAME', jobConfig['appBaseName'])
+            env('GEN_APK', jobConfig['generatedApkName'])
+            env('GRADLE_PROPERTIES', jobConfig['gradleProperties'])
+            env('KEY_STORE', jobConfig['keystore'])
+            env('KEY_STORE_FILE', jobConfig['keystoreFile'])
         }
 
-        // Run the scripts to build and rename the app
         steps {
+            // Run the scripts to build and rename the app
+            def copyKeyScript = readFileFromWorkspace('mobileApp/resources/copy_keys.sh')
             def buildScript = readFileFromWorkspace('mobileApp/resources/organize_android_app_build.sh')
+            def testSigningScript = readFileFromWorkspace('mobileApp/resources/check_release_build.sh')
+            // Copy keystores into workspace for release builds
+            if (jobConfig['release']) {
+                shell(copyKeyScript)
+            }
             shell(buildScript)
+            // Verify that they are signed properly for release builds
+            if (jobConfig['release']) {
+                shell(testSigningScript)
+            }
         }
 
         publishers {
@@ -127,22 +158,22 @@ secretMap.each { jobConfigs ->
 
             // Publish the application to HockeyApp
             configure { project ->
-                project / publishers << 'hockeyapp.HockeyappRecorder' {
+                project / publishers << 'hockeyapp.HockeyappRecorder' (schemaVersion: '2') {
                     applications {
-                        'hockeyapp.HockeyappApplication' {
+                        'hockeyapp.HockeyappApplication' (plugin: 'hockeyapp@1.2.1', schemaVersion: '1') {
                             apiToken jobConfig['hockeyAppApiToken']
                             notifyTeam true
                             filePath 'artifacts/*.apk'
                             dowloadAllowed true
-                            releaseNotesMethod {
-                                releaseNotes "${RELEASE_NOTES}"
+                            releaseNotesMethod (class: "net.hockeyapp.jenkins.releaseNotes.ManualReleaseNotes") {
+                                releaseNotes "\${RELEASE_NOTES}"
                                 isMarkDown false
                             }
+                            uploadMethod (class: 'net.hockeyapp.jenkins.uploadMethod.AppCreation') {}
                         }
                     }
                 }
             }
-
         }
     }
 }
