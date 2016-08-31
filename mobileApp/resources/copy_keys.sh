a2a621f67b59a18737be6ff32e63607f5986d9e8
stu <stucontact@gmail.com>
2016-08-31 12:20:12 -0400

    use credentials binding and fix resource scripts

--- mobileApp/resources/copy_keys.sh
+++ mobileApp/resources/copy_keys.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/bash env
+#!/usr/bin/env bash -e
 
 # Copy gradle.properties and the keystore file into the application directory
 # They will be used by the gradle script that builds the app
 cp $GRADLE_PROPERTIES $WORKSPACE/$APP_BASE_DIR/gradle.properties
 cp $KEY_STORE $WORKSPACE/$APP_BASE_DIR/$KEY_STORE_FILE
+chmod u+w $WORKSPACE/$APP_BASE_DIR/gradle.properties
