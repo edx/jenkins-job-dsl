30308e0d3365c59868913b03a7d3673c7b6889b5
Kevin Falcone <kevin@edx.org>
2016-09-08 21:51:07 -0400

    Copy assume-role for now
    resources aren't available in the generated job's workspace.
    We need to either stuff it in with EnvInject, or put it in a git repo
    we check out into the workspace all the time.  After that we can
    better factor this code and make ourselves happier.
    
    Longer plan is to put it in configuration.git:util/jenkins/
    and source it from scripts, but that will be a seperate PR.

--- devops/resources/run-ansible.sh
+++ devops/resources/run-ansible.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 cd $WORKSPACE/configuration
 pip install -r requirements.txt
@@ -9,15 +9,34 @@ export AWS_PROFILE=$deployment_tag
 
 env
 
-. util.sh
+
+set +x
+# Need to get util.sh in the workspace
+# . util.sh
+assume-role(){
+    ROLE_ARN="${1}"
+    SESSIONID=$(date +"%s")
+
+    RESULT=(`aws sts assume-role --role-arn $ROLE_ARN \
+            --role-session-name $SESSIONID \
+            --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
+            --output text`)
+
+    export AWS_ACCESS_KEY_ID=${RESULT[0]}
+    export AWS_SECRET_ACCESS_KEY=${RESULT[1]}
+    export AWS_SECURITY_TOKEN=${RESULT[2]}
+    export AWS_SESSION_TOKEN=${AWS_SECURITY_TOKEN}
+}
 
 # Assume the role that will allow running ec2.py for getting a dynamic inventory
 assume-role ${ROLE_ARN}
 
+set -x
+
 cd $WORKSPACE/configuration/playbooks
 
 # Pattern must be supplied explicitly as we take a conservative
-# approach given that ec2.py will provide a dynamic inventor
+# approach given that ec2.py will provide a dynamic inventory
 if [[ -z "${PATTERN}" ]]; then
   ANSIBLE_PATTERN="-i ${PATTERN}"
 else
@@ -31,7 +50,7 @@ else
 fi
 
 if [[ -z "${BECOME_USER}" ]]; then
-  ANSIBLE_BECOME=" -become --become-user=${BECOME_USER} "
+  ANSIBLE_BECOME=" --become --become-user=${BECOME_USER} "
 fi
 
 ansible ${ANSIBLE_PATTERN} ${ANSIBLE_INVENTORY} -u ${ANSIBLE_SSH_USER} ${ANSIBLE_BECOME} -m ${ANSIBLE_MODULE_NAME} \
