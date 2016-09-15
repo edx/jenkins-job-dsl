21fc009d0a0022c1ebe887896a7cc3fc7637b34b
Kevin Falcone <kevin@edx.org>
2016-09-15 13:24:13 -0400

    Allow for getting the inventory from the outptut of a script
    Useful if ec2.py doesn't return the set of hosts you need.
    
    This is a shell one liner, need to quote it to ensure proper passing to ansible

--- devops/resources/run-ansible.sh
+++ devops/resources/run-ansible.sh
@@ -48,9 +48,14 @@ else
   ANSIBLE_INVENTORY="-i ./ec2.py"
 fi
 
+if [[ -n ${CUSTOM_INVENTORY} ]]; then
+  HOSTS=$($CUSTOM_INVENTORY)
+  ANSIBLE_INVENTORY="-i ${HOSTS}"
+fi
+
 if [[ -n "${BECOME_USER}" ]]; then
   ANSIBLE_BECOME=" --become --become-user=${BECOME_USER} "
 fi
 
 ansible ${ANSIBLE_PATTERN} ${ANSIBLE_INVENTORY} -u ${ANSIBLE_SSH_USER} ${ANSIBLE_BECOME} -m ${ANSIBLE_MODULE_NAME} \
--a ${ANSIBLE_MODULE_ARGS}
+-a "${ANSIBLE_MODULE_ARGS}"
