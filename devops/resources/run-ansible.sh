6b6e20142e312b22e4d8ac2275909886fa68deb6
Kevin Falcone <kevin@edx.org>
2016-09-30 16:58:34 -0400

    Remove code we'll want in a lot of jobs

--- devops/resources/run-ansible.sh
+++ devops/resources/run-ansible.sh
@@ -8,28 +8,10 @@ pip install awscli
 
 env
 
-
+. util/jenkins/assume-role.sh
 set +x
-# Need to get util.sh in the workspace
-# . util.sh
-assume-role(){
-    ROLE_ARN="${1}"
-    SESSIONID=$(date +"%s")
-
-    RESULT=(`aws sts assume-role --role-arn $ROLE_ARN \
-            --role-session-name $SESSIONID \
-            --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
-            --output text`)
-
-    export AWS_ACCESS_KEY_ID=${RESULT[0]}
-    export AWS_SECRET_ACCESS_KEY=${RESULT[1]}
-    export AWS_SECURITY_TOKEN=${RESULT[2]}
-    export AWS_SESSION_TOKEN=${AWS_SECURITY_TOKEN}
-}
-
 # Assume the role that will allow running ec2.py for getting a dynamic inventory
 assume-role ${ROLE_ARN}
-
 set -x
 
 cd $WORKSPACE/configuration/playbooks
