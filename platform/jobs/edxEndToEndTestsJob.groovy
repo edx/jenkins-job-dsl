24c732825b420564cb93036e759ced7fbb740dae
stu <stucontact@gmail.com>
2017-02-02 15:51:24 -0500

    refactor e2e jobs for pr triggers

--- platform/jobs/edxEndToEndTestsJob.groovy
+++ platform/jobs/edxEndToEndTestsJob.groovy
@@ -4,9 +4,6 @@ import org.yaml.snakeyaml.Yaml
 import static org.edx.jenkins.dsl.JenkinsPublicConstants.JENKINS_PUBLIC_LOG_ROTATOR
 import static org.edx.jenkins.dsl.JenkinsPublicConstants.JENKINS_PUBLIC_WORKER
 
-// The edx-e2e-tests job is run automatically on every deployment of the edx-platform
-// from the gocd pipeline.
-
 /* stdout logger */
 /* use this instead of println, because you can pass it into closures or other scripts. */
 Map config = [:]
@@ -16,11 +13,14 @@ PrintStream out = config['out']
 
 /* Map to hold the k:v pairs parsed from the secret file */
 Map mailingListMap = [:]
+Map ghprbMap = [:]
 try {
     out.println('Parsing secret YAML file')
     String mailingListSecretContents  = new File("${MAILING_LIST_SECRET}").text
+    String ghprbConfigContents = new File("${GHPRB_SECRET}").text
     Yaml yaml = new Yaml()
     mailingListMap = yaml.load(mailingListSecretContents)
+    ghprbMap = yaml.load(ghprbConfigContents)
     out.println('Successfully parsed secret YAML file')
 }
 catch (any) {
@@ -30,67 +30,117 @@ catch (any) {
 }
 
 assert mailingListMap.containsKey('e2e_test_mailing_list')
+assert ghprbMap.containsKey('admin')
+assert ghprbMap.containsKey('userWhiteList')
+assert ghprbMap.containsKey('orgWhiteList')
 
-job('edx-e2e-tests') {
+// The edx-e2e-tests job is run automatically on every deployment of the edx-platform
+// from the gocd pipeline.
+Map pipelineJob = [ name: 'edx-e2e-tests',
+                    trigger: 'manual',
+                    branch: '*/master',
+                    description: 'Run end-to-end tests against GoCD deployments',
+                    courseNumber: 'AR-1000'
+                    ]
 
-    description('Run end-to-end tests against an instance of the edx-platform')
+// The edx-e2e-tests_pr job is run on every PR to the edx-e2e-tests repo. This
+// is used for development of the tests themselves
+Map prJob = [ name: 'edx-e2e-tests-pr',
+              trigger: 'ghprb',
+              branch: '${ghprbActualCommit}',
+              description: 'Verify the quality of changes made to the end-to-end tests',
+              courseNumber: 'AR-1001'
+              ]
 
-    authorization {
-        blocksInheritance(true)
-        permissionAll('edx')
-    }
+List jobConfigs = [ pipelineJob, prJob ]
 
-    logRotator JENKINS_PUBLIC_LOG_ROTATOR()
-    label(JENKINS_PUBLIC_WORKER)
-    // Disable concurrent builds because the environment in use is a shared
-    // resource, and concurrent builds can cause spurious test results
-    concurrentBuild(false)
+jobConfigs.each { jobConfig ->
+    job(jobConfig.name) {
 
-    parameters {
-        stringParam('COURSE_ORG', 'ArbiRaees', 'Organization name of the course')
-        stringParam('COURSE_NUMBER', 'AR-1000', 'Course number')
-        stringParam('COURSE_RUN', 'fall', 'Term in which course will run')
-        stringParam('COURSE_DISPLAY_NAME', 'Manual Smoke Test Course 1 - Auto', 'Display name of the course')
-    }
+        description(jobConfig.description)
 
-    scm {
-        git {
-            remote {
-                url('https://github.com/edx/edx-e2e-tests.git')
+        authorization {
+            blocksInheritance(true)
+            permissionAll('edx')
+        }
+
+        if (jobConfig.trigger == 'ghprb') {
+            properties {
+                githubProjectUrl('https://github.com/edx/edx-e2e-tests')
             }
-            branch('*/master')
-            browser()
         }
-    }
 
-    wrappers {
-        timeout {
-            absolute(75)
-        }
-        timestamps()
-        colorizeOutput('gnome-terminal')
-        credentialsBinding {
-            string('BASIC_AUTH_USER', 'BASIC_AUTH_USER')
-            string('BASIC_AUTH_PASSWORD', 'BASIC_AUTH_PASSWORD')
-            string('USER_LOGIN_EMAIL', 'USER_LOGIN_EMAIL')
-            string('USER_LOGIN_PASSWORD', 'USER_LOGIN_PASSWORD')
-        }
-    }
+        logRotator JENKINS_PUBLIC_LOG_ROTATOR()
+        label(JENKINS_PUBLIC_WORKER)
+        // Disable concurrent builds because the environment in use is a shared
+        // resource, and concurrent builds can cause spurious test results
+        concurrentBuild(false)
 
-    steps {
-        shell('jenkins/end_to_end_tests.sh')
-    }
-
-    publishers {
-        archiveJunit('reports/*.xml') {
-            allowEmptyResults(false)
+        parameters {
+            stringParam('COURSE_ORG', 'ArbiRaees', 'Organization name of the course')
+            stringParam('COURSE_NUMBER', jobConfig.courseNumber, 'Course number')
+            stringParam('COURSE_RUN', 'fall', 'Term in which course will run')
+            stringParam('COURSE_DISPLAY_NAME', 'Manual Smoke Test Course 1 - Auto', 'Display name of the course')
         }
-        archiveArtifacts {
-            pattern('reports/*.xml')
-            pattern('log/*')
-            pattern('screenshots/*')
-        }
-        mailer(mailingListMap['e2e_test_mailing_list'])
-    }
 
+        scm {
+            git {
+                remote {
+                    url('https://github.com/edx/edx-e2e-tests.git')
+                    refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+                }
+                branch(jobConfig.branch)
+                browser()
+            }
+        }
+
+        // enable triggers for the PR job
+        if (jobConfig.trigger == "ghprb") { 
+            triggers {
+                pullRequest {
+                    admins(ghprbMap['admin'])
+                    useGitHubHooks()
+                    triggerPhrase('jenkins run e2e')
+                    userWhitelist(ghprbMap['userWhiteList'])
+                    orgWhitelist(ghprbMap['orgWhiteList'])
+                    extensions {
+                        commitStatus {
+                            context('jenkins/e2e')
+                        }
+                    }
+                }
+            }
+        }
+
+        wrappers {
+            timeout {
+                absolute(75)
+            }
+            timestamps()
+            colorizeOutput('gnome-terminal')
+            credentialsBinding {
+                string('BASIC_AUTH_USER', 'BASIC_AUTH_USER')
+                string('BASIC_AUTH_PASSWORD', 'BASIC_AUTH_PASSWORD')
+                string('USER_LOGIN_EMAIL', 'USER_LOGIN_EMAIL')
+                string('USER_LOGIN_PASSWORD', 'USER_LOGIN_PASSWORD')
+            }
+        }
+
+        steps {
+            shell('jenkins/end_to_end_tests.sh')
+        }
+
+        publishers {
+            archiveJunit('reports/*.xml') {
+                allowEmptyResults(false)
+            }
+            archiveArtifacts {
+                pattern('reports/*.xml')
+                pattern('log/*')
+                pattern('screenshots/*')
+            }
+            mailer(mailingListMap['e2e_test_mailing_list'])
+        }
+
+    }
 }
