2ef7cebe9aaba09c07a150dcffa9a251da9aa0d1
Jeff LaJoie <jlajoie@edx.org>
2017-02-06 15:36:09 -0500

    initial build sandbox for ora2

---
+++ ora2/jobs/BuildOra2Sandbox.groovy
@@ -0,0 +1,42 @@
+/*
+    Variables consumed from the EXTRA_VARS input to your seed job in addition
+    to those listed in the seed job.
+
+    * NOTIFY_ON_FAILURE : e-mail-contact@example.com
+    * SANDBOX_JOB : 'Sandboxes/CreateSandbox'
+*/
+
+package ora2.jobs
+
+class BuildOra2Sandbox {
+
+    public static def job = { dslFactory, extraVars ->
+        return dslFactory.job(extraVars.get('JOB_FOLDER_NAME', 'ora2') + '/add-course-to-sandbox') {
+            properties {
+                parameters {
+                    stringParam('dns_name', 'ora2', '')
+                    stringParam('name_tag', 'ora2', '')
+                    stringParam('configuration_version', 'master', '')
+                    stringParam('configuration_source_repo', 'https://github.com/edx/configuration.git')
+                    stringParam('configuration_secure_version', 'master', '')
+                    stringParam('coniguration_internal_version', 'master', '')
+                    stringParam('edxapp_version', 'master', '')
+                    stringParam('edx_platform_repo', 'https://github.com/edx/edx-platform.git', '')
+                }
+            }
+            steps {
+                downstreamParameterized {
+                    triggers(extraVars.get('SANDBOX_JOB','Sandboxes/CreateSandbox')) {
+                        parameters {
+                            currentBuild()
+                            booleanParam('basic_auth', false)
+                        }
+                    }
+                }
+            }
+            publishers {
+                mailer(extraVars['NOTIFY_ON_FAILURE'], true, false)
+            }
+        }
+    }
+}
