be631d0263da1b0b729697119167d8b334ddfee6
Jeff LaJoie <jlajoie@edx.org>
2017-02-07 09:15:37 -0500

    renaming

--- ora2/jobs/BuildOra2Sandbox.groovy
+++ ora2/jobs/BuildOra2Sandbox.groovy
@@ -12,7 +12,7 @@ package ora2.jobs
 class BuildOra2Sandbox {
 
     public static def job = { dslFactory, extraVars ->
-        return dslFactory.job(extraVars.get('JOB_FOLDER_NAME', 'ora2') + '/build-ora-sandbox') {
+        return dslFactory.job(extraVars.get('JOB_FOLDER_NAME', 'ora2') + '/build-ora2-sandbox') {
             properties {
                 parameters {
                     stringParam('dns_name', 'ora2', '')
