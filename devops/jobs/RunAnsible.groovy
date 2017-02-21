828edb1522080a96e5ca616668b1e4484cc4e2b7
Kevin Falcone <kevin@edx.org>
2017-02-21 14:17:06 -0500

    Ensure we can support prod-edge and prod-edx in this job

--- devops/jobs/RunAnsible.groovy
+++ devops/jobs/RunAnsible.groovy
@@ -3,7 +3,8 @@
     Variables consumed from the EXTRA_VARS input to your seed job in addition
     to those listed in the seed job.
 
-    SSH_AGENT_KEY: dictionary of key per environment (looked up from the currently generating ENVIRONMENT)
+    SSH_AGENT_KEY: dictionary of key per environment-deployment (looked up from the currently generating ENVIRONMENT and DEPLOYMENT)
+        As a backwards compatibility shim, will also fall back to environment only until all deployed jobs are updated.
     CONFIGURATION_REPO: standard default available
     CONFIGURATION_BRANCH: defaults to master
     ACCESS_CONTROL: list of Jenkins matrix targets who should get read access (edx*team or username)
@@ -51,7 +52,13 @@ class RunAnsible {
                     file('AWS_CONFIG_FILE', extraVars.get('AWS_CONFIG_FILE','jenkins-aws-credentials'))
                     string('ROLE_ARN',extraVars.get('AWS_ROLE_ARN','find-host-role-arn'))
                 }
-                sshAgent(extraVars.get("SSH_AGENT_KEY").get(environment))
+                def ssh_key = extraVars["SSH_AGENT_KEY"]["${environment}-${deployment}"]
+                if (!ssh_key) {
+                  // Backcompat shim until we reseed all the jobs and rename all the credentials
+                  ssh_key = extraVars["SSH_AGENT_KEY"][environment]
+                }
+                assert ssh_key : "Unable to find an ssh key in SSH_AGENT_KEY.${environment}-${deployment} or SSH_AGENT_KEY.${environment}"
+                sshAgent(ssh_key)
             }
 
             parameters {
