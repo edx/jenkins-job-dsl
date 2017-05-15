99c6c6d9317d919d376ce27a2321b33acde1926b
Michael Youngstrom <myoungstrom@edx.org>
2017-05-15 10:55:07 -0400

    Add DSL for toggle-webhook-interceptor job
    nits
    
    Change to bool types
    
    update script argument and tweak description
    
    change input variable
    
    pull out region
    
    change env var name

---
+++ testeng/jobs/toggleWebhookInterceptor.groovy
@@ -0,0 +1,103 @@
+package testeng
+
+import hudson.util.Secret
+import org.yaml.snakeyaml.Yaml
+import static org.edx.jenkins.dsl.JenkinsPublicConstants.JENKINS_PUBLIC_MASKED_PASSWORD
+
+Map config = [:]
+Binding bindings = getBinding()
+config.putAll(bindings.getVariables())
+PrintStream out = config['out']
+
+try {
+    out.println('Parsing secret YAML file')
+    String constantsConfig = new File("${EDX_TOGGLE_INTERCEPTOR_SECRET}").text
+    Yaml yaml = new Yaml()
+    secretMap = yaml.load(constantsConfig)
+    out.println('Successfully parsed secret YAML file')
+}
+catch (any) {
+    out.println('Jenkins DSL: Error parsing secret YAML file')
+    out.println('Exiting with error code 1')
+    return 1
+}
+
+/*
+    sampleJobConfig:
+        toolsTeam: [ member1, member2, ... ]
+        region: us-east-1
+        accessKeyId: 123abc
+        secretAccessKey: 123abc
+        email: email@address
+*/
+
+// Iterate over the job configurations
+secretMap.each { jobConfigs ->
+    Map jobConfig = jobConfigs.getValue()
+
+    assert jobConfig.containsKey('toolsTeam')
+    assert jobConfig.containsKey('region')
+    assert jobConfig.containsKey('accessKeyId')
+    assert jobConfig.containsKey('secretAccessKey')
+    assert jobConfig.containsKey('email')
+
+    job('toggle-webhook-interceptor') {
+
+        description('Update the state of the webhook-processor.')
+
+        // Enable project security to avoid exposing aws keys
+        authorization {
+            blocksInheritance(true)
+            jobConfig['toolsTeam'].each { member ->
+                permissionAll(member)
+            }
+        }
+
+        parameters {
+            choiceParam('IS_ENABLED', ['True', 'False'],
+                        'Whether or not the webhook-processor should be enabled')
+        }
+
+        concurrentBuild(false)
+
+        scm {
+            git {
+                remote {
+                    url('https://github.com/edx/testeng-ci.git')
+                }
+                branch('*/master')
+                browser()
+            }
+        }
+
+        // Put sensitive info into masked password slots
+        configure { project ->
+            project / buildWrappers << 'EnvInjectPasswordWrapper' {
+                injectGlobalPasswords false
+                maskPasswordParameters true
+                passwordEntries {
+                    EnvInjectPasswordEntry {
+                        name 'AWS_ACCESS_KEY_ID'
+                        value Secret.fromString(jobConfig['accessKeyId']).getEncryptedValue()
+                    }
+                    EnvInjectPasswordEntry {
+                        name 'AWS_SECRET_ACCESS_KEY'
+                        value Secret.fromString(jobConfig['secretAccessKey']).getEncryptedValue()
+                    }
+                }
+            }
+        }
+
+        environmentVariables {
+            env('AWS_DEFAULT_REGION', jobConfig['region'])
+        }
+
+        steps {
+            shell(readFileFromWorkspace('testeng/resources/toggle-interceptor.sh'))
+        }
+
+        publishers {
+            mailer(jobConfig['email'])
+        }
+    }
+}
