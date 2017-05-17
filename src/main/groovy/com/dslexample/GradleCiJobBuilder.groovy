ebed09cbd4808ede03be1dcda24a2e995e8391ea
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-05-16 23:58:03 -0400

    Added seed job and updated docker-compose.yml
    A seed job has been added from https://github.com/sheehan/job-dsl-gradle-example. This allows developers to run a command against the Jenkins REST API to create the seed job rather than manually creating the job.
    
    Additionally, the Docker Compose file has been updated to use the official Jenkins image. It is unclear what happened to the original Dockerfile.
    
    LEARNER-509

---
+++ src/main/groovy/com/dslexample/GradleCiJobBuilder.groovy
@@ -0,0 +1,47 @@
+package com.dslexample
+
+import javaposse.jobdsl.dsl.DslFactory
+import javaposse.jobdsl.dsl.Job
+
+/**
+ * Example Class for creating a Gradle build
+ */
+class GradleCiJobBuilder {
+
+    String name
+    String description
+    String ownerAndProject
+    String gitBranch = 'master'
+    String pollScmSchedule = '@daily'
+    String tasks
+    String switches
+    Boolean useWrapper = true
+    String junitResults = '**/build/test-results/*.xml'
+    String artifacts = '**/build/libs/*.jar'
+    List<String> emails = []
+
+    Job build(DslFactory dslFactory) {
+        dslFactory.job(name) {
+            it.description this.description
+            logRotator {
+                numToKeep 5
+            }
+            scm {
+                github this.ownerAndProject, gitBranch
+            }
+            triggers {
+                scm pollScmSchedule
+            }
+            steps {
+                gradle tasks, switches, useWrapper
+            }
+            publishers {
+                archiveArtifacts artifacts
+                archiveJunit junitResults
+                if (emails) {
+                    mailer emails.join(' ')
+                }
+            }
+        }
+    }
+}
