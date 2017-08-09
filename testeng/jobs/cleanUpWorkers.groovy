d860c4330b389f55a7b72e5d755807af8c159c1d
Michael Youngstrom <myoungstrom@edx.org>
2017-08-09 14:34:08 -0400

    Add dsl for clean-up-workers job
    add groovy script and log rotator
    
    add testeng package
    
    config
    
    remove config

---
+++ testeng/jobs/cleanUpWorkers.groovy
@@ -0,0 +1,33 @@
+package testeng
+
+job('clean-up-workers') {
+
+    logRotator {
+        daysToKeep(10)
+        numToKeep(25)
+    }
+
+    label('micro-worker')
+
+    scm {
+        git {
+            remote {
+                url('https://github.com/edx/testeng-ci.git')
+            }
+            branch('*/master')
+            browser()
+        }
+    }
+
+    triggers{
+        cron('H H/6 * * *')
+    }
+
+    steps {
+        systemGroovyScriptFile('jenkins/admin-scripts/delete-old-workers.groovy')
+    }
+
+    publishers {
+        mailer('testeng@edx.org')
+    }
+}
