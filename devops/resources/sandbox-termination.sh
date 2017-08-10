1e388349e865fc0f9aa093c6017ea0414a238e0c
syed-awais-ali <awaisalisabir@gmail.com>
2017-08-10 20:30:25 +0500

    added argument to script

--- devops/resources/sandbox-termination.sh
+++ devops/resources/sandbox-termination.sh
@@ -21,7 +21,7 @@ if [ "$NOOP" = true ]; then
   extra_args="-n"
 fi
 
-python terminate-sandbox.py $extra_args -z $ROUTE53_ZONE -r $AWS_REGION --hipchat_room $HIPCHAT_ROOM --hipchat_api_key $HIPCHAT_API_KEY
+python terminate-sandbox.py $extra_args -z $ROUTE53_ZONE -r $AWS_REGION --hipchat_room $HIPCHAT_ROOM --hipchat_api_key $HIPCHAT_API_KEY --edx_git_bot_token $EDX_GIT_BOT_TOKEN
 
 curl https://nosnch.in/c6dcca38ad
 
