630e80511989bf9559cd2229bfa7da38169f10ea
Stuart Young <syoung@edx.org>
2017-08-14 12:23:52 -0400

    dsl script for oep2 job

---
+++ testeng/jobs/oep2Report.groovy
@@ -0,0 +1,56 @@
+package testeng
+
+job('oep2-report') {
+
+    description('Generate a report describing OEP-2 compliance accross edX repos')
+    authorization {
+        blocksInheritance(true)
+        permissionAll('edx')
+        permission('hudson.model.Item.Discover', 'anonymous')
+    }
+    concurrentBuild(false)
+    label('jenkins-worker')
+    scm {
+        git {
+            remote {
+                url('https://github.com/edx/repo-tools')
+            }
+            branch('*/master')
+            browser()
+        }
+    }
+    triggers {
+        cron('@midnight')
+    }
+    wrappers {
+        timestamps()
+        sshAgent('jenkins')
+        credentialsBinding {
+           file('OEP_REPORT_TOKEN', 'GITHUB_STATUS_OAUTH_TOKEN')
+        }
+    }
+    steps {
+        virtualenv {
+            name('oep-venv')
+            nature('shell')
+            clear(true)
+            command(readFileFromWorkspace('testeng/resources/create-oep-report.sh'))
+        }
+    }
+    publishers {
+        archiveJunit('oep2-report.xml') {
+            retainLongStdout(false)
+            healthScaleFactor(1.0)
+        }
+        publishHtml {
+            report('.') {
+                allowMissing(true)
+                alwaysLinkToLastBuild(false)
+                keepAll(true)
+                reportFiles('oep2.report.html')
+                reportName('OEP 2 Report')
+            }
+        }
+    }
+
+}
