23055628a956440a410f7ef98d172aeded4eac04
syed-awais-ali <awaisalisabir@gmail.com>
2017-08-21 17:56:52 +0500

    jenkins job and script for cloudflare

---
+++ devops/jobs/CloudFlareHitRate.groovy
@@ -0,0 +1,75 @@
+/*
+
+    Variables without defaults are marked (required)
+
+    Variables consumed for this job:
+        * NOTIFY_ON_FAILURE: (Required - email address)
+        * SECURE_GIT_CREDENTIALS: (Required - jenkins name of git credentials)
+        * ZONE_ID: (Required - The CloudFlare Zone ID)
+        * AUTH_KEY: (Required - Authentication key for the account that would make API calls)
+        * EMAIL: (Required - Email of the account for making API calls)
+
+*/
+package devops.jobs
+
+
+import static org.edx.jenkins.dsl.DevopsTasks.common_parameters
+
+
+class CloudflareHitRate {
+    public static def job = { dslFactory, extraVars ->
+        assert extraVars.containsKey('ZONE_ID') : "Zone id of domain"
+        assert extraVars.containsKey('AUTH_KEY') : "Authentication key for Cloudflare"
+        assert extraVars.containsKey('EMAIL') : "e-mail for authentication to Cloudflare API"
+
+        dslFactory.job(extraVars.get("FOLDER_NAME","Monitoring") + "/cloudflare-hit-rate-edx") {
+
+
+            def zone_id = extraVars.get('ZONE_ID')
+            def auth_key = extraVars.get('AUTH_KEY')
+            def email = extraVars.get('EMAIL')
+
+
+            triggers {
+                cron("H/8 * * * *")
+            }
+
+
+            steps {
+                virtualenv {
+                    pythonName('System-CPython-3.5')
+                    nature("shell")
+                    systemSitePackages(false)
+
+                    command(
+                        dslFactory.readFileFromWorkspace("devops/resources/cloudflare-hit-rate.sh")
+                    )
+
+                }
+
+            }
+
+            multiscm{
+                git {
+                    remote {
+                        url('https://github.com/edx/configuration.git')
+                        branch('master')
+                    }
+                    extensions {
+                        cleanAfterCheckout()
+                        pruneBranches()
+                        relativeTargetDirectory('configuration')
+                    }
+                }
+            }
+
+            parameters common_parameters(extraVars)
+
+
+            publishers {
+                mailer(extraVars.get('NOTIFY_ON_FAILURE'), false, false)
+            }
+
+        }
+    }
+}
---
+++ devops/resources/cloudflare-hit-rate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -exuo pipefail
+
+cd $WORKSPACE/configuration
+env
+
+python util/jenkins/cloudflare-hit-rate-monitor.py\
+    --zone ${ZONE_ID}\
+    --auth-key ${AUTH_KEY}\
+    --email ${EMAIL}
