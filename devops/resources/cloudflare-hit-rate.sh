abf958031d540483016b3528d93eda0b27b9bbba
syed-awais-ali <awaisalisabir@gmail.com>
2017-09-13 21:35:33 +0500

    fix path for requirements

--- devops/resources/cloudflare-hit-rate.sh
+++ devops/resources/cloudflare-hit-rate.sh
@@ -2,7 +2,7 @@
 set -exuo pipefail
 
 cd $WORKSPACE/configuration
-pip install -r requirements-cloudflare.txt
+pip install -r util/jenkins/requirements-cloudflare.txt
 env
 
 python util/jenkins/cloudflare-hit-rate.py\
