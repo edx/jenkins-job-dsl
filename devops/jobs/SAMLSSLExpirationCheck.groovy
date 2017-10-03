db82009fb34a1781db38d7c86a87e0ac1c705dd5
nadeemshahzad <nshahzad@edx.org>
2017-10-03 18:34:37 +0500

    fixes

--- devops/jobs/SAMLSSLExpirationCheck.groovy
+++ devops/jobs/SAMLSSLExpirationCheck.groovy
@@ -7,29 +7,20 @@
             environments:
               environment (required)
                  saml_cert_file (required)
-    * FROM_ADDRESS: email address to send notifications that certificates are expiring
-    * TO_ADDRESS: email address that sends the mail
+                 configuration_secure_repo (required)
     * SECURE_GIT_CREDENTIALS: secure-bot-user (required)
     * SYSADMIN_REPO: repository containing SSL expiration check python script (required)
     * SYSADMIN_BRANCH: default is master
     * CONFIGURATION_REPO: name of config repo, default is https://github.com/edx/configuration.git
     * CONFIGURATION_BRANCH: default is master
-    * CONFIGURATION_SECURE_REPO: name of config secure repo
-    * CONFIGURATION_SECURE_BRANCH: default is master
     * REGION: default is us-east-1
     * NOTIFY_ON_FAILURE: alert@example.com
     * FOLDER_NAME: folder, default is Monitoring
     * DAYS: alert if SSL certificate will expire within these days
-    
-
- This job expects the following credentials to be defined on the folder
-    tools-edx-jenkins-aws-credentials: file with key/secret in boto config format
-    saml-ssl-expiration-check-${deployment}-role-arn: the role to aws sts assume-role
 
 */
 
 package devops.jobs
-import static org.edx.jenkins.dsl.Constants.common_wrappers
 import static org.edx.jenkins.dsl.Constants.common_logrotator
 
 class SAMLSSLExpirationCheck{
@@ -39,50 +30,25 @@ class SAMLSSLExpirationCheck{
         extraVars.get('DEPLOYMENTS').each { deployment, configuration ->
                 configuration.environments.each { environment, inner_config -> 
                     dslFactory.job(extraVars.get("FOLDER_NAME","Monitoring") + "/saml-ssl-expiration-check-${environment}-${deployment}") {
-                        wrappers common_wrappers
                         logRotator common_logrotator
 
-                        wrappers {
-                            credentialsBinding {
-                            file('AWS_CONFIG_FILE','tools-edx-jenkins-aws-credentials')
-                            string('ROLE_ARN', "saml-ssl-expiration-check-${deployment}-role-arn")
-                            }
-                        }
 
                         def gitCredentialId = extraVars.get('SECURE_GIT_CREDENTIALS','')
-                        def config_secure_repo = "git@github.com:edx-ops/${deployment}-secure.git"
-                        extraVars['CONFIGURATION_SECURE_REPO'] = config_secure_repo
+                        
                         assert extraVars.containsKey('SYSADMIN_REPO') : "Please define a system admin repo where the SSL expiration check  script is located"
 
                         parameters{
-                        stringParam('CONFIGURATION_REPO', extraVars.get('CONFIGURATION_REPO', 'https://github.com/edx/configuration.git'),
-                            'Git repo containing edX configuration.')
-                        stringParam('CONFIGURATION_BRANCH', extraVars.get('CONFIGURATION_BRANCH', 'master'),
-                            'e.g. tagname or origin/branchname')
                         stringParam('SYSADMIN_REPO', extraVars.get('SYSADMIN_REPO'),
                             'Git repo containing sysadmin configuration which contains the ssl expiration check script.')
                         stringParam('SYSADMIN_BRANCH', extraVars.get('SYSADMIN_BRANCH', 'master'),
                             'e.g. tagname or origin/branchname')
-                        stringParam('CONFIGURATION_SECURE_REPO', extraVars.get('CONFIGURATION_SECURE_REPO'),
+                        stringParam('CONFIGURATION_SECURE_REPO', inner_config.get('configuration_secure_repo'),
                             'Git repo containing secure configuration.')
-                        stringParam('CONFIGURATION_SECURE_BRANCH', extraVars.get('CONFIGURATION_SECURE_BRANCH', 'master'),
+                        stringParam('CONFIGURATION_SECURE_BRANCH', inner_config.get('configuration_secure_branch', 'master'),
                             'e.g. tagname or origin/branchname')
-                        stringParam('TO_ADDRESS', extraVars.get('TO_ADDRESS', ""))
-                        stringParam('FROM_ADDRESS', extraVars.get('FROM_ADDRESS', ""))
                         }
                 
                         multiscm{
-                            git {
-                                remote {
-                                    url('$CONFIGURATION_REPO')
-                                    branch('$CONFIGURATION_BRANCH')
-                                }
-                                extensions {
-                                    cleanAfterCheckout()
-                                    pruneBranches()
-                                    relativeTargetDirectory('configuration')
-                                }     
-                            }
                             git {
                                 remote {
                                     url('$SYSADMIN_REPO')
@@ -126,6 +92,7 @@ class SAMLSSLExpirationCheck{
 
                         steps {
                             virtualenv {
+                                pythonName('System-CPython-3.5')
                                 nature("shell")
                                 systemSitePackages(false)
 
--- devops/resources/saml-ssl-expiration-check.sh
+++ devops/resources/saml-ssl-expiration-check.sh
@@ -7,20 +7,10 @@ HOME=/edx/var/jenkins
 env
 set -x
 
-cd $WORKSPACE/configuration
-pip install -r requirements.txt
-. util/jenkins/assume-role.sh
-
-assume-role ${ROLE_ARN}
-
-SAML_SSL_CERT_FILE = $WORKSPACE/configuration_secure/ansible/vars/${SAML_CERT_FILE}
+SAML_SSL_CERT_FILE=$WORKSPACE/configuration_secure/ansible/vars/${SAML_CERT_FILE}
 
 cd $WORKSPACE/sysadmin
 pip install -r requirements.txt
 cd jenkins
 
-if [[ -n "${FROM_ADDRESS}" && "${TO_ADDRESS}" ]]; then
-	python saml-ssl-expiration-check.py --region $REGION -d $DAYS -i $SAML_SSL_CERT_FILE  -r $TO_ADDRESS -f $FROM_ADDRESS
-else
-	python saml-ssl-expiration-check.py --region $REGION -d $DAYS -i $SAML_SSL_CERT_FILE
-fi
+python saml-ssl-expiration-check.py --region $REGION -d $DAYS -i $SAML_SSL_CERT_FILE
