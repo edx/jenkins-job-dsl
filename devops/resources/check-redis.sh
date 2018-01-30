f5463e3850e8f25e848916dcabf68f11b9cd0991
Joseph Mulloy <jmulloy@edx.org>
2018-01-30 10:50:06 -0500

    Add custom thresholds support OPS-2721

--- devops/resources/check-redis.sh
+++ devops/resources/check-redis.sh
@@ -5,6 +5,12 @@ cd $WORKSPACE/configuration/util/jenkins
 pip install -r requirements-celery.txt
 env
 
+# Set THRESHOLDS if not set in job, need because we're setting -u
+# Otherwise we get an error "THRESHOLDS: unbound variable"
+if [[ ! -v THRESHOLDS ]]; then
+    THRESHOLDS=""
+fi
+
 . ./assume-role.sh
 assume-role ${ROLE_ARN}
-python ./check-celery-queues.py --environment ${ENVIRONMENT} --deploy ${DEPLOYMENT} --host ${REDIS_HOST} --sns-arn ${SNS_TOPIC}
+python ./check-celery-queues.py --environment ${ENVIRONMENT} --deploy ${DEPLOYMENT} --host ${REDIS_HOST} --sns-arn ${SNS_TOPIC} ${THRESHOLDS}
