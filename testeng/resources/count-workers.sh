62a4babda9207ee10ab935db35c490fcdd47ed02
Michael Youngstrom <myoungstrom@edx.org>
2018-02-26 12:43:31 -0500

    Create count-workers job to monitor jenkins load

---
+++ testeng/resources/count-workers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+virtualenv workers_venv -q
+source workers_venv/bin/activate
+
+echo "Installing python requirements..."
+pip install -q -r requirements.txt
+
+echo "Counting workers for build jenkins..."
+python jenkins/workers.py -j https://build.testeng.edx.org
