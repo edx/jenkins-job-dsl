9fc68a6193f62dc21f3e0af9622c4d31683dea31
Michael Youngstrom <myoungstrom@edx.org>
2018-03-20 13:04:37 -0400

    add git clone for master

--- platform/jobs/edxPlatformLettuceMaster.groovy
+++ platform/jobs/edxPlatformLettuceMaster.groovy
@@ -115,6 +115,27 @@ jobConfigs.each { jobConfig ->
                     url("https://github.com/edx/${jobConfig.repoName}.git")
                     refspec(jobConfig.refSpec)
                     if (!jobConfig.open.toBoolean()) {
+                        credentials('EDX_STATUS_BOT_CREDENTIALS')
+                    }
+                }
+                branch(jobConfig.defaultBranch)
+                browser()
+                extensions {
+                    cloneOptions {
+                        // Use a reference clone for quicker clones. This is configured on jenkins workers via
+                        // (https://github.com/edx/configuration/blob/master/playbooks/roles/test_build_server/tasks/main.yml#L26)
+                        reference("\$HOME/edx-platform-clone")
+                        timeout(10)
+                    }
+                    cleanBeforeCheckout()
+                    relativeTargetDirectory(jobConfig.repoName)
+                }
+            }
+            git {
+                remote {
+                    url("https://github.com/edx/${jobConfig.repoName}.git")
+                    refspec(jobConfig.refSpec)
+                    if (!jobConfig.open.toBoolean()) {
                         credentials("EDX_STATUS_BOT_CREDENTIALS")
                     }
                 }
