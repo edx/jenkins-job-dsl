e5c3d60e963dc67064b67cac82fc432c8e5390ee
Fred Smith <derf@edx.org>
2018-06-11 10:56:17 -0400

    edx-east cleanup

--- devops/resources/run-app-permissions.sh
+++ devops/resources/run-app-permissions.sh
@@ -10,7 +10,7 @@ env
 assume-role ${ROLE_ARN}
 cd $WORKSPACE/configuration/playbooks
 
-INVENTORY=$(../active_instances_in_asg.py --asg ${ENVIRONMENT}-${DEPLOYMENT}-worker)
+INVENTORY=$(./active_instances_in_asg.py --asg ${ENVIRONMENT}-${DEPLOYMENT}-worker)
 if [[ -n ${INVENTORY} ]]; then
     ansible-playbook -i ${INVENTORY} manage_edxapp_users_and_groups.yml -e@$WORKSPACE/app-permissions/${ENVIRONMENT}-${DEPLOYMENT}-edxapp.yml --user ${USER}
 else
