c13257c7bbef56411f84be596dcb11ed9be95c11
Abdul Mannan <raoabdulmannan@gmail.com>
2018-09-28 18:51:08 +0500

    rewrite enrollment dsl

--- dataeng/jobs/analytics/Enrollment.groovy
+++ dataeng/jobs/analytics/Enrollment.groovy
@@ -1,78 +1,31 @@
 package analytics
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_multiscm
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_parameters
+import static org.edx.jenkins.dsl.AnalyticsConstants.date_interval_parameters
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_log_rotator
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_wrappers
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
 
 class Enrollment {
     public static def job = { dslFactory, extraVars ->
-        dslFactory.job('enrollment') {
-            parameters {
-                stringParam('CONF_BRANCH', '$ANALYTICS_CONFIGURATION_RELEASE', 'Branch from the edx-analytics-configuration repository. For tags use tags/[tag-name].')
-                stringParam('CLUSTER_NAME', 'EnrollmentTest', 'Name of the EMR cluster to provision for this job.')
-                stringParam('NUM_TASK_CAPACITY', '45', 'Used to specify spot capacity of instance fleets core, also used to calculate the number of reducers by doubling this amount.')
-                stringParam('NUM_REDUCE_TASKS', '90', 'Number of reducers for mapreduce job.')
-                booleanParam('TERMINATE', true, 'Boolean indicating whether to terminate the cluster on job completion or not.')
-                stringParam('FROM_DATE', '2013-11-01', 'The first date to export data for. Data for this date and all days before the "TO_DATE" parameter will be included.')
-                stringParam('TO_DATE', 'today', 'The day after the last date to export data for. Data from the "FROM_DATE" parameter to 11:59:59 on the date before this date will be included.')
-                stringParam('TASK_USER', '$AUTOMATION_USER')
-                stringParam('SECURE_REPO', extraVars.get('SECURE_REPO_URL'))
-                stringParam('SECURE_BRANCH', '$ANALYTICS_SECURE_RELEASE', 'Branch from the analytics-secure repository, where the configuration settings reside. For tags use tags/[tag-name]')
-                stringParam('SECURE_CONFIG', 'analytics-tasks/dev-edx.cfg', 'Config file from the analytics-secure repository for this job.')
-                stringParam('NOTIFY', '$PAGER_NOTIFY', 'Space separated list of emails to notify in case of failure.')
-                stringParam('EXTRA_ARGS', '')
-                stringParam('TASKS_BRANCH', '$ANALYTICS_PIPELINE_RELEASE', 'Branch from the edx-analytics-pipeline repository. For tags use tags/[tag-name].')
-                // EMR cluster configuration
-                stringParam('EMR_MASTER_INSTANCE_TYPE', 'm4.2xlarge', 'EC2 Instance type used for master.')
-                stringParam('EMR_MASTER_USE_SPOT_INSTANCE', 'False', 'Use spot instances.')
-                stringParam('EMR_WORKER_INSTANCE_TYPE_1', 'm4.2xlarge', 'EC2 instance type used by workers.')
-                stringParam('EMR_WORKER_INSTANCE_TYPE_2', 'm4.4xlarge', 'EC2 instance type used by workers.')
-                stringParam('EMR_ADDITIONAL_APPLICATION_PROPERTIES', ' ', 'Additional configuration properties for applications. Use blank space as default value.')
-                stringParam('EMR_MAPRED_SITE_PROPERTIES', ' ', 'Additional hadoop mapred-site properties. Use blank space as default value.')
-                stringParam('EMR_YARN_SITE_PROPERTIES', ' ', 'Additional hadoop yarn-site properties. Use blank space as default value.')
-                stringParam('EMR_USER_INFO', ' ', 'Additional github users with ssh access to cluster. Use blank space as default value.')
-                textParam('EXTRA_VARS', extraVars.get('AWS_EXTRA_VARS', ''))
-                // END EMR cluster configuration
-            }
-
-            logRotator {
-                daysToKeep(365)
-            }
-
-            multiscm{
-                git {
-                    remote {
-                        url('git@github.com:edx/edx-analytics-pipeline.git')
-                        branch('$TASKS_BRANCH')
-                        credentials('1')
-                    }
-                    extensions {
-                        pruneBranches()
-                        relativeTargetDirectory('analytics-tasks')
-                    }
+        extraVars.get('ENVIRONMENTS').each { environment, env_config ->
+            dslFactory.job("enrollment-$environment") {
+                logRotator common_log_rotator(extraVars)
+                parameters common_parameters(extraVars, env_config)
+                parameters date_interval_parameters(extraVars)
+                parameters {
+                    stringParam('EXTRA_ARGS', '')
                 }
-                git {
-                    remote {
-                        url('git@github.com:edx/edx-analytics-configuration.git')
-                        branch('$CONF_BRANCH')
-                        credentials('1')
-                    }
-                    extensions {
-                        pruneBranches()
-                        relativeTargetDirectory('analytics-configuration')
-                    }
+                multiscm common_multiscm(extraVars)
+                triggers {
+                    cron(env_config.get('JOB_FREQUENCY', ''))
                 }
-            }
-
-            wrappers {
-                timestamps()
-                sshAgent('1') // use credential's custom ID
-            }
-
-            steps {
-                shell(dslFactory.readFileFromWorkspace("dataeng/resources/enrollment.sh"))
-            }
-
-            publishers {
-                // Mark the build as 'unstable' if the text is found in 'console log'.
-                textFinder("\\[Traceback\\]", '', true, false, true)
-                mailer('$NOTIFY')
+                wrappers common_wrappers(extraVars)
+                publishers common_publishers(extraVars)
+                steps {
+                    shell(dslFactory.readFileFromWorkspace('dataeng/resources/enrollment.sh'))
+                    shell('curl https://nosnch.in/' + env_config.get('SNITCH', ''))
+                }
             }
         }
     }
