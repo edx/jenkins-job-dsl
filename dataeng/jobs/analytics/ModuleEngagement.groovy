57acb5700951ab136cd4a153e7f5df0c0e6351d3
Hassan Javeed <hassan@edx.org>
2018-10-12 18:25:18 +0500

    Enrollment and Module-Engagement Job DSL.

---
+++ dataeng/jobs/analytics/ModuleEngagement.groovy
@@ -0,0 +1,28 @@
+package analytics
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_multiscm
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_parameters
+import static org.edx.jenkins.dsl.AnalyticsConstants.date_interval_parameters
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_log_rotator
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_wrappers
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
+
+class ModuleEngagement {
+    public static def job = { dslFactory, allVars ->
+        allVars.get('ENVIRONMENTS').each { environment, env_config ->
+            dslFactory.job("module-engagement-$environment") {
+                logRotator common_log_rotator(allVars)
+                parameters common_parameters(allVars, env_config)
+                parameters date_interval_parameters(allVars)
+                multiscm common_multiscm(allVars)
+                wrappers common_wrappers(allVars)
+                publishers common_publishers(allVars)
+                steps {
+                    shell(dslFactory.readFileFromWorkspace('dataeng/resources/module-engagement.sh'))
+                    if (env_config.get('SNITCH')) {
+                        shell('curl https://nosnch.in/' + env_config.get('SNITCH'))
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ dataeng/resources/enrollment.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -z "$NUM_REDUCE_TASKS" ]; then
+    NUM_REDUCE_TASKS=$(( $NUM_TASK_CAPACITY*2 ))
+fi
+
+env
+
+${WORKSPACE}/analytics-configuration/automation/run-automated-task.sh \
+  ImportEnrollmentsIntoMysql --local-scheduler \
+  --interval $(date +%Y-%m-%d -d "$FROM_DATE")-$(date +%Y-%m-%d -d "$TO_DATE") \
+  --n-reduce-tasks $NUM_REDUCE_TASKS \
+  --overwrite-mysql
