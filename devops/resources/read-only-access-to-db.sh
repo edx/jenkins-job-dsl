edb41d28e0b89b63b57587a989d2111a5bfdb52c
Cory Lee <cory@edx.org>
2018-10-26 14:29:54 -0400

    Fail add keys job when keys missing

--- devops/resources/read-only-access-to-db.sh
+++ devops/resources/read-only-access-to-db.sh
@@ -6,4 +6,4 @@ cd $WORKSPACE/configuration
 pip install -r requirements.txt
 
 cd playbooks
-ansible-playbook -i "$BASTION_HOST," tools-gp.yml -u ubuntu -e${USERS_YAML} --tags users 
+ansible-playbook -i "$BASTION_HOST," tools-gp.yml -u ubuntu -e${USERS_YAML} -eUSER_FAIL_MISSING_KEYS=true --tags users 
