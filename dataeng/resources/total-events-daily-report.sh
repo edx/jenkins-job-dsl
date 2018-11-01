81c7ac216e98e1218bfd6812710d998ebe657d52
Hassan Javeed <hassan@edx.org>
2018-11-01 19:40:16 +0500

    Total Events Daily report DSL.

---
+++ dataeng/resources/total-events-daily-report.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ -z "$NUM_REDUCE_TASKS" ]; then
+    NUM_REDUCE_TASKS=$(( $NUM_TASK_CAPACITY + 1 ))
+fi
+
+rm -rf venvs
+
+env|sort
+DAY_TO_REPORT_DATE_FORMATTED=$(date +%Y-%m-%d -d "$DAY_TO_REPORT")
+
+${WORKSPACE}/analytics-configuration/automation/run-automated-task.sh \
+  TotalEventsReportWorkflow --local-scheduler \
+  --interval $DAY_TO_REPORT_DATE_FORMATTED \
+  --n-reduce-tasks $NUM_REDUCE_TASKS \
+  --report $S3_DIR/reports/$DAY_TO_REPORT_DATE_FORMATTED \
+  --counts $S3_DIR/counts/$DAY_TO_REPORT_DATE_FORMATTED/
