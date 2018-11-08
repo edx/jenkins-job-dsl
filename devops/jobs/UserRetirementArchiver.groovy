089cb38abaa7ff08026fccab2cda7c5a5e7c9dd3
Troy Sankey <tsankey@edx.org>
2018-11-08 16:18:04 -0500

    Pivot to making the archiver DSL fanout to multiple jobs

--- devops/jobs/UserRetirementArchiver.groovy
+++ devops/jobs/UserRetirementArchiver.groovy
@@ -2,20 +2,27 @@
 
 Defines the job for archiving retired user statuses.
 
-Variables consumed for this job:
-  * MAILING_LIST: A string containing a comma-delimited list of emails to notify when a build fails.
+Job config variables consumed for this job (via extraVars):
+  * ENVIRONMENT_DEPLOYMENT: The environment and deployment written as $environment-$deployment
+  * FOLDER_NAME: Name of jenkins "folder" under which to put this job.
   * SECURE_GIT_CREDENTIALS: Name of jenkins git credential scoped to the UserRetirement "folder" in jenkins.
-  * FOLDER_NAME: Name of jenkins "folder".
   * ACCESS_CONTROL: List of github groups with read and build access to this job.
   * ADMIN_ACCESS_CONTROL: List of github groups with full admin access to this job.
+  * CRON: Optional. Defines the cron line for scheduling the job.
+  * MAILING_LIST: Optional. A string containing a comma-delimited list of emails to notify when a build fails.
 
-This job expects the following credentials to be defined on the folder
+This job expects the following credentials to be defined on the folder:
   * tools-edx-jenkins-aws-credentials: file with key/secret in boto config format
-  * tools-jenkins-janitor-${deployment}-role-arn: the role to aws sts assume-role
+  * ${environment}-${deployment}-retirement-archive-upload-role: the role to aws sts assume-role
 
 */
 package devops.jobs
-import static org.edx.jenkins.dsl.DevopsConstants.common_read_permissions
+import static org.edx.jenkins.dsl.UserRetirementConstants.common_access_controls
+import static org.edx.jenkins.dsl.UserRetirementConstants.common_multiscm
+import static org.edx.jenkins.dsl.UserRetirementConstants.common_parameters
+import static org.edx.jenkins.dsl.UserRetirementConstants.common_publishers
+import static org.edx.jenkins.dsl.UserRetirementConstants.common_triggers
+import static org.edx.jenkins.dsl.UserRetirementConstants.common_wrappers
 
 class UserRetirementArchiver {
     public static def job = { dslFactory, extraVars ->
@@ -25,86 +32,43 @@ class UserRetirementArchiver {
             "Make sure MAILING_LIST is a single string (containing a comma-delimited list) and not a proper Yaml " +
             "list or some other non-string type."
 
-        dslFactory.job(extraVars.get("FOLDER_NAME") + "/user-retirement-archiver") {
+        def job_name = extraVars.get("FOLDER_NAME") + '/archiver-' + extraVars.get('ENVIRONMENT_DEPLOYMENT')
+        dslFactory.job(job_name) {
+
             description('Archive the old user retirement status rows to AWS S3.')
+
+            // First we write the DSL statements which should be common to ALL user retirement jobs.
+            authorization common_access_controls(extraVars)
+            triggers common_triggers(extraVars)
+            wrappers common_wrappers(extraVars)
+            parameters common_parameters(extraVars)
+            checkoutRetryCount(5)  // retry cloning repositories
+            multiscm common_multiscm(extraVars)
+            publishers common_publishers(extraVars)
+            logRotator {
+                daysToKeep(30) // keep jobs around for 30 days.
+            }
+
+            ////
+            // Now, everything which follows is custom to this particular job.
+            ////
 
             // Only one of these jobs should be running at a time.
             concurrentBuild(false)
 
-            // keep jobs around for 30 days.
-            logRotator {
-                daysToKeep(30)
-            }
-
-            extraVars.get('ACCESS_CONTROL',[]).each { acl ->
-                common_read_permissions.each { perm ->
-                    authorization {
-                        permission(perm,acl)
-                    }
-                }
-            }
-
-            extraVars.get('ADMIN_ACCESS_CONTROL',[]).each { acl ->
-                authorization {
-                    permissionAll(acl)
-                }
-            }
-
             wrappers {
-                buildUserVars() /* gives us access to BUILD_USER_ID, among other things */
-                buildName('#${BUILD_NUMBER}, ${ENV,var="ENVIRONMENT"}')
-                timestamps()
-                colorizeOutput('xterm')
                 credentialsBinding {
                     file('AWS_CONFIG_FILE','tools-edx-jenkins-aws-credentials')
-                    string('ROLE_ARN', "${environment}-${deployment}-retirement-archive-upload-role")
+                    string('ROLE_ARN', extraVars.get('ENVIRONMENT_DEPLOYMENT') + '-retirement-archive-upload-role')
                 }
             }
-
-            def gitCredentialId = extraVars.get('SECURE_GIT_CREDENTIALS','')
 
             parameters {
-                stringParam('TUBULAR_BRANCH', 'master', 'Repo branch for the tubular scripts.')
-                stringParam('USER_RETIREMENT_SECURE_BRANCH', 'master', 'Repo branch for the tubular scripts.')
-                stringParam('ENVIRONMENT', '', 'edx environment from which to archive user retirements, in ENVIRONMENT-DEPLOYMENT format. (Required)')
-                stringParam('COOL_OFF_DAYS', '67', 'Number of days after retirement request in which a retired learner status should be archived in S3.')
-            }
-
-            // retry cloning repositories
-            checkoutRetryCount(5)
-
-            multiscm {
-                git {
-                    remote {
-                        url('git@github.com:edx-ops/user-retirement-secure.git')
-                        if (gitCredentialId) {
-                            credentials(gitCredentialId)
-                        }
-                    }
-                    branch('$USER_RETIREMENT_SECURE_BRANCH')
-                    extensions {
-                        relativeTargetDirectory('user-retirement-secure')
-                        cloneOptions {
-                            shallow()
-                            timeout(10)
-                        }
-                        cleanBeforeCheckout()
-                    }
-                }
-                git {
-                    remote {
-                        url('https://github.com/edx/tubular.git')
-                    }
-                    branch('$TUBULAR_BRANCH')
-                    extensions {
-                        relativeTargetDirectory('tubular')
-                        cloneOptions {
-                            shallow()
-                            timeout(10)
-                        }
-                        cleanBeforeCheckout()
-                    }
-                }
+                stringParam(
+                    'COOL_OFF_DAYS',
+                    '67',
+                    'Number of days after retirement request when a user retirement status should be archived in S3.'
+                )
             }
 
             steps {
@@ -114,31 +78,6 @@ class UserRetirementArchiver {
                     nature('shell')
                     systemSitePackages(false)
                     command(readFileFromWorkspace('devops/resources/user-retirement-archiver.sh'))
-                }
-            }
-
-            publishers {
-                // After all the build steps have completed, cleanup the workspace in
-                // case this worker instance is re-used for a different job.
-                wsCleanup()
-
-                if (extraVars.containsKey('MAILING_LIST')) {
-                    // Send an alerting email upon failure.
-                    extendedEmail {
-                        recipientList(extraVars.get('MAILING_LIST'))
-                        triggers {
-                            failure {
-                                attachBuildLog(false)  // build log contains PII!
-                                compressBuildLog(false)  // build log contains PII!
-                                subject('Failed build: user-retirement-archiver #${BUILD_NUMBER}')
-                                content('Build #${BUILD_NUMBER} failed.\n\nSee ${BUILD_URL} for details.')
-                                contentType('text/plain')
-                                sendTo {
-                                    recipientList()
-                                }
-                            }
-                        }
-                    }
                 }
             }
         }
---
+++ src/main/groovy/org/edx/jenkins/dsl/UserRetirementConstants.groovy
@@ -0,0 +1,112 @@
+/*
+ * Common DSL closures for all User Retirement jobs.
+ */
+package org.edx.jenkins.dsl
+
+class UserRetirementConstants {
+    // All retirement jobs have two elevated levels of access control:
+    //   1) read/build (determined by the ACCESS_CONTROL job config variable)
+    //   2) admin (determined by the ADMIN_ACCESS_CONTROL job config variable)
+    public static def common_access_controls = { extraVars ->
+        def read_build_permission_names = [
+            'hudson.model.Item.Build',
+            'hudson.model.Item.Cancel',
+            'hudson.model.Item.Discover',
+            'hudson.model.Item.Read',
+        ]
+        def read_build_perms = extraVars.get('ACCESS_CONTROL',[]).each { acl ->
+            read_build_permission_names.each { perm ->
+                permission(perm,acl)
+            }
+        }
+        def admin_perms = extraVars.get('ADMIN_ACCESS_CONTROL',[]).each { acl ->
+            permissionAll(acl)
+        }
+        // The ">>" operator composes two closures into one closure.
+        return read_build_perms >> admin_perms
+    }
+
+    public static def common_triggers = { extraVars ->
+        if (extraVars.containsKey('CRON')) {
+            cron(extraVars.get('CRON'))
+        }
+    }
+
+    public static def common_wrappers = { extraVars ->
+        return {
+            buildUserVars() /* gives us access to BUILD_USER_ID, among other things */
+            buildName('#${BUILD_NUMBER}, ' + extraVars.get('ENVIRONMENT_DEPLOYMENT'))
+            timestamps()
+            colorizeOutput('xterm')
+        }
+    }
+
+    public static def common_parameters = { extraVars ->
+        return {
+            stringParam('TUBULAR_BRANCH', 'master', 'Repo branch for the tubular scripts.')
+            stringParam('USER_RETIREMENT_SECURE_BRANCH', 'master', 'Repo branch for the tubular scripts.')
+        }
+    }
+
+    public static def common_multiscm = { extraVars ->
+        return {
+            git {
+                remote {
+                    url('git@github.com:edx-ops/user-retirement-secure.git')
+                    if (extraVars.containsKey('SECURE_GIT_CREDENTIALS')) {
+                        credentials(extraVars.get('SECURE_GIT_CREDENTIALS'))
+                    }
+                }
+                branch('$USER_RETIREMENT_SECURE_BRANCH')
+                extensions {
+                    relativeTargetDirectory('user-retirement-secure')
+                    cloneOptions {
+                        shallow()
+                        timeout(10)
+                    }
+                    cleanBeforeCheckout()
+                }
+            }
+            git {
+                remote {
+                    url('https://github.com/edx/tubular.git')
+                }
+                branch('$TUBULAR_BRANCH')
+                extensions {
+                    relativeTargetDirectory('tubular')
+                    cloneOptions {
+                        shallow()
+                        timeout(10)
+                    }
+                    cleanBeforeCheckout()
+                }
+            }
+        }
+    }
+    public static def common_publishers = { extraVars ->
+        return {
+            // After all the build steps have completed, cleanup the workspace in
+            // case this worker instance is re-used for a different job.
+            wsCleanup()
+
+            if (extraVars.containsKey('MAILING_LIST')) {
+                // Send an alerting email upon failure.
+                extendedEmail {
+                    recipientList(extraVars.get('MAILING_LIST'))
+                    triggers {
+                        failure {
+                            attachBuildLog(false)  // build log contains PII!
+                            compressBuildLog(false)  // build log contains PII!
+                            subject('Failed build: ${JOB_NAME} #${BUILD_NUMBER}')
+                            content('Build #${BUILD_NUMBER} failed.\n\nSee ${BUILD_URL} for details.')
+                            contentType('text/plain')
+                            sendTo {
+                                recipientList()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
