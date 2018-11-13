8a1dee96d2756712192446341cb8903de0ba096b
Hassan Javeed <hassan@edx.org>
2018-11-13 19:43:30 +0500

    Emr Cost reporter dSL.

---
+++ dataeng/resources/emr-cost-reporter.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -ex
+
+rm -rf /tmp/ecc-venv
+mkdir /tmp/ecc-venv
+virtualenv /tmp/ecc-venv
+. /tmp/ecc-venv/bin/activate
+
+cd emr-cost-calculator
+pip install -r requirements.txt
+
+# Calculate costs for all clusters that were created in the last 24 hours
+COST=$(python emr_cost_calculator.py total --output cloudwatch --output text --output graphite --graphite-host $GRAPHITE_HOST)
+
+# If today is Tuesday, then we are analyzing clusters from Monday, when we run weekly jobs, so we raise the threshold accordingly
+if [ "x$(date +%u)" = "x2" ]
+then
+    echo "Today is tuesday, adjusting the threshold up by \$${WEEKLY_JOB_THRESHOLD_ADJUSTMENT} to account for weekly jobs that run on Monday."
+    THRESHOLD=$((${THRESHOLD} + ${WEEKLY_JOB_THRESHOLD_ADJUSTMENT}))
+fi
+
+if [ "$(echo "${COST}>${THRESHOLD}" | bc)" = "1" ]
+then
+    echo "Actual cost \$${COST} exceeded the adjusted threshold of \$${THRESHOLD}, failing."
+    exit 1
+else
+    exit 0
+fi
