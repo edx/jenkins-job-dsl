2aa633c44e7c8367e1ef5323e031ba986ca7a809
Hassan Javeed <hassan@edx.org>
2018-12-03 19:30:15 +0500

    single-sql-script DSL.

--- dataeng/jobs/analytics/SqlScripts.groovy
+++ dataeng/jobs/analytics/SqlScripts.groovy
@@ -7,16 +7,21 @@ import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_triggers
 
 class SqlScripts {
-    public static def job = { dslFactory, allVars ->
+
+    public static def sql_script_params = { allVars ->
+        return {
+            stringParam('SCHEMA', allVars.get('SCHEMA'))
+            stringParam('CREDENTIALS', allVars.get('CREDENTIALS'))
+            stringParam('SCRIPTS_REPO', allVars.get('SCRIPTS_REPO_URL'))
+            stringParam('SCRIPTS_BRANCH', 'origin/master')
+            stringParam('EXTRA_ARGS', '')
+        }
+    }
+
+    public static def multiple_scripts_job = { dslFactory, allVars ->
         dslFactory.job("sql-scripts") {
             logRotator common_log_rotator(allVars)
-            parameters {
-                stringParam('SCHEMA', allVars.get('SCHEMA'))
-                stringParam('CREDENTIALS', allVars.get('CREDENTIALS'))
-                stringParam('SCRIPTS_REPO', allVars.get('SCRIPTS_REPO_URL'))
-                stringParam('SCRIPTS_BRANCH', 'origin/master')
-                stringParam('EXTRA_ARGS', '')
-            }
+            parameters SqlScripts.sql_script_params(allVars)
             parameters common_parameters(allVars)
             multiscm common_multiscm(allVars)
             triggers common_triggers(allVars)
@@ -27,6 +32,24 @@ class SqlScripts {
             }
             steps {
                 shell(dslFactory.readFileFromWorkspace('dataeng/resources/sql-scripts.sh'))
+            }
+        }
+    }
+
+    public static def single_script_job = { dslFactory, allVars ->
+        dslFactory.job("single-sql-script") {
+            logRotator common_log_rotator(allVars)
+            parameters SqlScripts.sql_script_params(allVars)
+            parameters {
+                stringParam('SOURCE_SCRIPT', '', 'Path to the source script, relative to the root of the repository. Most scripts just require a script name.')
+                stringParam('SCRIPT_NAME')
+            }
+            parameters common_parameters(allVars)
+            multiscm common_multiscm(allVars)
+            wrappers common_wrappers(allVars)
+            publishers common_publishers(allVars)
+            steps {
+                shell(dslFactory.readFileFromWorkspace('dataeng/resources/single-sql-script.sh'))
             }
         }
     }
