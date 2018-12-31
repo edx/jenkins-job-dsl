3dead0ef3582fee0a12b3c0a5b9806556310ec11
Abdul Mannan <raoabdulmannan@gmail.com>
2018-12-31 18:42:57 +0500

    Load insights to vertica DSL

--- dataeng/jobs/analytics/LoadInsightsToVertica.groovy
+++ dataeng/jobs/analytics/LoadInsightsToVertica.groovy
@@ -1,18 +1,16 @@
 package analytics
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_multiscm
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_parameters
-import static org.edx.jenkins.dsl.AnalyticsConstants.to_date_interval_parameter
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_log_rotator
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_wrappers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_triggers
 
-class LoadVideoToVertica {
+class LoadInsightsToVertica {
     public static def job = { dslFactory, allVars ->
-        dslFactory.job("load-video-to-vertica") {
+        dslFactory.job("load-insights-to-vertica") {
             logRotator common_log_rotator(allVars)
             parameters common_parameters(allVars)
-            parameters to_date_interval_parameter(allVars)
             parameters {
                 stringParam('SCHEMA', allVars.get('SCHEMA'))
                 stringParam('MARKER_SCHEMA', allVars.get('MARKER_SCHEMA'))
@@ -23,7 +21,7 @@ class LoadVideoToVertica {
             wrappers common_wrappers(allVars)
             publishers common_publishers(allVars)
             steps {
-                shell(dslFactory.readFileFromWorkspace('dataeng/resources/load-video-to-vertica.sh'))
+                shell(dslFactory.readFileFromWorkspace('dataeng/resources/load-insights-to-vertica.sh'))
             }
         }
     }
--- dataeng/resources/load-insights-to-vertica.sh
+++ dataeng/resources/load-insights-to-vertica.sh
@@ -3,11 +3,9 @@
 env
 
 ${WORKSPACE}/analytics-configuration/automation/run-automated-task.sh \
- LoadAllVideoToVertica --local-scheduler \
- --date $(date +%Y-%m-%d -d "$TO_DATE") \
+ LoadInsightsTableToVertica --local-scheduler \
  --schema $SCHEMA \
  --credentials $CREDENTIALS \
  --marker-schema $MARKER_SCHEMA \
  --overwrite \
  $EXTRA_ARGS
-
