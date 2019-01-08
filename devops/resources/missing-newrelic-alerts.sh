d96117480a747230d7fd80676e9308ffc8891cb7
syedimranhassan <imran@HammadHussain-Laptop-128.arbisoft.com>
2019-01-08 13:50:35 +0500

    Add new jobs for missing_newrelic_alerts

--- devops/resources/missing-newrelic-alerts.sh
+++ devops/resources/missing-newrelic-alerts.sh
@@ -3,9 +3,11 @@
 cd $WORKSPACE/configuration/util/jenkins/
 
 pip install -r check_celery_progress/requirements.txt
+. ./assume-role.sh
 
-. util/jenkins/assume-role.sh
 # Assume the role
+set +x
 assume-role ${ROLE_ARN}
+set -x
 
 python missing_alerts_checker.py --new-relic-api-key ${NEW_RELIC_API_KEY}
