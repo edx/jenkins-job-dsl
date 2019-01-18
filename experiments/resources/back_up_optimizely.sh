d81ce256000f461602ebf410f0c9f5b15ca38b67
Joseph Mulloy <jmulloy@edx.org>
2019-01-18 16:04:50 -0500

    Add dsl and shell script for jenkins job to back up optimizely history.
    REVEM-147

---
+++ experiments/resources/back_up_optimizely.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -exuo pipefail
+
+# Required by click http://click.pocoo.org/5/python3/
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+export GIT_AUTHOR_NAME
+export GIT_AUTHOR_EMAIL
+export GIT_COMMITTER_NAME
+export GIT_COMMITTER_EMAIL
+
+
+cd $WORKSPACE/py-opt-cli
+pip install -r requirements.txt
+pip install -e .
+env
+
+cd $WORKSPACE/optimizely-experiments
+py-opt-cli pull
+
+git add -A
+git commit -am "history commit job # ${BUILD_ID}"
+git push origin HEAD:history
