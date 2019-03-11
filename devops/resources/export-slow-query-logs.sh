974d804fc66bf9fb855177c8a4baa6e3c61e2fe3
syedimranhassan <imran.hassan@arbisoft.com>
2019-03-11 17:27:02 +0500

    Job for slow query logs to cloudwatch

---
+++ devops/resources/export-slow-query-logs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $WORKSPACE/configuration/util/jenkins/export_slow_logs
+
+pip install -r requirements.txt
+. ../assume-role.sh
+
+# Assume the role
+set +x
+assume-role ${ROLE_ARN}
+set -x
+
+python export_slow_query_logs.py
