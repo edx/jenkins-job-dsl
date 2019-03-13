d2ca9e033b4c9a996a2e9b9f5964a1c6808e87f3
syedimranhassan <imran.hassan@arbisoft.com>
2019-03-13 17:33:20 +0500

    Jenkins DSL Job for read table size

---
+++ devops/jobs/CheckTableSize.groovy
@@ -0,0 +1,68 @@
+package devops.jobs
+import static org.edx.jenkins.dsl.Constants.common_wrappers
+
+class EXPORTRDSSLOWQUERYLOGS {
+    public static def job = { dslFactory, extraVars ->
+        assert extraVars.containsKey("DEPLOYMENTS") : "Please define DEPLOYMENTS. It should be list of strings."
+        assert !(extraVars.get("DEPLOYMENTS") instanceof String) : "Make sure DEPLOYMENTS is a list of string"
+
+        extraVars.get('DEPLOYMENTS').each { deployment, configuration ->
+
+                dslFactory.job(extraVars.get("FOLDER_NAME","Monitoring") + "/table-size-monitoring-${deployment}") {
+                    parameters {
+                        stringParam('CONFIGURATION_REPO', 'https://github.com/edx/configuration.git')
+                        stringParam('CONFIGURATION_BRANCH', 'master')
+                    }
+
+                    wrappers common_wrappers
+
+                    wrappers {
+                        credentialsBinding {
+                            file("AWS_CONFIG_FILE","tools-edx-jenkins-aws-credentials")
+                            def variable = "${deployment}-table-size-monitoring"
+                            string("ROLE_ARN", variable)
+                        }
+                    }
+
+                    triggers {
+                        cron("H * * * *")
+                    }
+
+                    environmentVariables {
+                        env('AWS_DEFAULT_REGION', extraVars.get('REGION'))
+                        env('USERNAME', extraVars.get('USERNAME'))
+                        env('PASSWORD', configuration.get('PASSWORD'))
+                    }
+
+                    multiscm {
+                        git {
+                            remote {
+                                url('$CONFIGURATION_REPO')
+                                branch('$CONFIGURATION_BRANCH')
+                            }
+                            extensions {
+                                cleanAfterCheckout()
+                                pruneBranches()
+                                relativeTargetDirectory('configuration')
+                            }
+                        }
+                    }
+                    steps {
+                        virtualenv {
+                            nature("shell")
+                            systemSitePackages(false)
+
+                            command(
+                                    dslFactory.readFileFromWorkspace("devops/resources/table-size-monitoring.sh")
+                            )
+                        }
+                    }
+
+                    publishers {
+                        mailer(extraVars.get('NOTIFY_ON_FAILURE'), false, false)
+
+                    }
+                }
+        }
+    }
+}
---
+++ devops/resources/table-size-monitoring.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $WORKSPACE/configuration/util/jenkins/check_table_size
+
+pip install -r requirements.txt
+. ../assume-role.sh
+
+# Assume the role
+set +x
+assume-role ${ROLE_ARN}
+set -x
+
+python check_table_size.py  --username ${USERNAME} --password ${PASSWORD}
