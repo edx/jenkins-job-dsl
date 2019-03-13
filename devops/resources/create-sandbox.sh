579db509ebc4f6c9e4752bb8ea054ef3623f029a
Shafqat Farhan <shafqat.farhan@arbisoft.com>
2019-03-13 20:51:50 +0500

    Modified create sandbox file for Apros Sandbox testing

--- devops/resources/create-sandbox.sh
+++ devops/resources/create-sandbox.sh
@@ -9,17 +9,17 @@ pip install awscli
 set +x
 
 
-SESSIONID=$(date +"%s")
+#SESSIONID=$(date +"%s")
 
-RESULT=(`aws sts assume-role --role-arn $ROLE_ARN \
-            --role-session-name $SESSIONID \
-            --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
-            --output text`)
+#RESULT=(`aws sts assume-role --role-arn $ROLE_ARN \
+#            --role-session-name $SESSIONID \
+#            --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
+#            --output text`)
 
-export AWS_ACCESS_KEY_ID=${RESULT[0]}
-export AWS_SECRET_ACCESS_KEY=${RESULT[1]}
-export AWS_SECURITY_TOKEN=${RESULT[2]}
-export AWS_SESSION_TOKEN=${AWS_SECURITY_TOKEN}
+#export AWS_ACCESS_KEY_ID=${RESULT[0]}
+#export AWS_SECRET_ACCESS_KEY=${RESULT[1]}
+#export AWS_SECURITY_TOKEN=${RESULT[2]}
+#export AWS_SESSION_TOKEN=${AWS_SECURITY_TOKEN}
 
 set -x
 
