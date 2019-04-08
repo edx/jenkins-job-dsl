3e3c241da239813f29a3c8db0f283494b123ab9d
Stuart Young <syoung@edx.org>
2019-04-08 09:34:55 -0400

    remove old android scripts

--- mobile_app/resources/path_constants.py
+++
@@ -1,9 +0,0 @@
-"""
-Path constants used by the mobile build scripts
-"""
-CONFIG_FILE = "CONFIGURATION"
-
-CODE_CHECKOUT_DIRECTORY = "code.git"
-CONFIG_CHECKOUT_DIRECTORY = "config.git"
-
-PROPERTIES_FILE = "edx.properties"
