72548f14e39d2e68dd4bb3ff0ec7af0a4111ab9d
syedimranhassan <imran.hassan@arbisoft.com>
2019-05-03 15:49:41 +0500

    Update messages for jenkins job

--- devops/resources/app-permission-runner-success.sh
+++ devops/resources/app-permission-runner-success.sh
@@ -3,4 +3,4 @@
 cd $WORKSPACE/tubular
 pip install -r requirements.txt
 
-python scripts/message_prs_in_range.py --org "edx" --repo "app-permissions" --base_sha ${GIT_PREVIOUS_COMMIT_1} --head_sha ${GIT_COMMIT_1} --release ${ENVIRONMENT} --extra_text "Job run successfully on ${ENVIRONMENT}-${DEPLOYMENT}"
+python scripts/message_prs_in_range.py --org "edx" --repo "app-permissions" --base_sha ${GIT_PREVIOUS_COMMIT_1} --head_sha ${GIT_COMMIT_1} --release "jenkins" --extra_text "on ${ENVIRONMENT}-${DEPLOYMENT}"
