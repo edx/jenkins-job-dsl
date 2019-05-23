de3141edd849cd47a6d6a101209b2f80e97860d3
Michael Youngstrom <youngstrom.m@husky.neu.edu>
2019-05-23 11:17:34 -0400

    fix

--- platform/jobs/edxPlatformJsPr.groovy
+++ platform/jobs/edxPlatformJsPr.groovy
@@ -229,7 +229,7 @@ jobConfigs.each { jobConfig ->
             }
             archiveJunit(JENKINS_PUBLIC_JUNIT_REPORTS)
             if (jobConfig.repoName == "edx-platform") {
-                downstreamParameterized JENKINS_EDX_PLATFORM_TEST_NOTIFIER.call("${jobConfig.repoName}", "${ghprbPullId}")
+                downstreamParameterized JENKINS_EDX_PLATFORM_TEST_NOTIFIER.call("${jobConfig.repoName}", '${ghprbPullId}')
             }
         }
     }
