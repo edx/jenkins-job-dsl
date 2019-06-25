cfb3c4336daf0a2e65f11e52ee47425f0a44b7d9
syed-awais-ali <awaisalisabir@gmail.com>
2019-06-25 16:10:24 +0500

    added params and cron timings

--- devops/resources/check_primary_keys.sh
+++ devops/resources/check_primary_keys.sh
@@ -5,9 +5,15 @@ cd $WORKSPACE/configuration/util/jenkins/primary_keys
 pip install -r requirements.txt
 . ../assume-role.sh
 
-# Assume the role
+# Assume role for different envs
 set +x
 assume-role ${ROLE_ARN}
 set -x
 
-python check_primary_keys.py
+# Set RDSIGNORE if not set in job, need because we're setting -u
+# Otherwise we get an error "RDSIGNORE: unbound variable"
+if [[ ! -v RDSIGNORE ]]; then
+    RDSIGNORE=""
+fi
+
+python ./check_primary_keys.py --environment ${ENVIRONMENT} --deploy ${DEPLOYMENT} --region $AWS_DEFAULT_REGION --recipient $TO_ADDRESS --sender $FROM_ADDRESS ${RDSIGNORE}
