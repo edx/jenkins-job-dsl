e423439ff8e98ecd12d8b6557f438532d38f7e08
syedimranhassan <imran.hassan@arbisoft.com>
2019-07-30 18:23:03 +0500

    Get splunk backup dir as parameter

--- devops/resources/backup-splunk-to-s3.sh
+++ devops/resources/backup-splunk-to-s3.sh
@@ -5,5 +5,6 @@ pip install -r requirements.txt
 
 cd $WORKSPACE/configuration/playbooks/edx-east
 
-ansible-playbook -u ubuntu -i $host_ip,  splunk_config_backup.yml -e "splunk_host_id=$splunk_host \
-      splunk_s3_backups_bucket=$s3_bucket"
+ansible-playbook -u splunkbackup -i $host_ip,  splunk_config_backup.yml -e "splunk_host_id=$splunk_host \
+      splunk_s3_backups_bucket=$s3_bucket \
+       splunk_backup_dir=$splunk_backup_dir"
