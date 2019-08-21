b7ee5c8ecb61cbabd376816708b1d8111f7dec81
Jeremy Bowman <jbowman@edx.org>
2019-08-21 12:11:33 -0400

    Fix more github_helpers import errors

--- testeng/resources/bokchoy-db-cache-script.sh
+++ testeng/resources/bokchoy-db-cache-script.sh
@@ -8,5 +8,4 @@ paver update_local_bokchoy_db_from_s3 --rewrite_fingerprint
 
 cd ../testeng-ci
 pip install -r requirements/base.txt
-cd jenkins
-python bokchoy_db_pull_request.py --sha $GIT_COMMIT --repo_root "../../edx-platform"
+python -m jenkins.bokchoy_db_pull_request --sha $GIT_COMMIT --repo_root "../edx-platform"
