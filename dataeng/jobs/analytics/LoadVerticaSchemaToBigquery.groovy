06e539c6ab4481687d6499cc6eb74510fff0fa84
Brian Wilson <brian@edx.org>
2019-09-25 16:32:18 -0400

    Break vertica export into three job definitions.

--- dataeng/jobs/analytics/LoadVerticaSchemaToBigquery.groovy
+++ dataeng/jobs/analytics/LoadVerticaSchemaToBigquery.groovy
@@ -6,31 +6,25 @@ import static org.edx.jenkins.dsl.AnalyticsConstants.common_wrappers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_triggers
 
-class LoadVerticaSchemaToSnowflake {
+class LoadVerticaSchemaToBigquery {
     public static def job = { dslFactory, allVars ->
         allVars.get('VERTICA_SCHEMAS').each { schema, schema_config ->
-            dslFactory.job("load-vertica-schema-to-snowflake-$schema") {
+            dslFactory.job("load-vertica-schema-to-bigquery-$schema") {
                 logRotator common_log_rotator(allVars, schema_config)
                 parameters common_parameters(allVars, schema_config)
                 parameters {
                     stringParam('RUN_DATE', schema_config.get('RUN_DATE', allVars.get('RUN_DATE', 'today')), 'Run date for the job. A string that can be parsed by the GNU coreutils "date" utility.')
                     stringParam('OVERWRITE', schema_config.get('OVERWRITE', allVars.get('OVERWRITE')), 'Set to: --overwrite if you want to enable overwrite.')
-                    stringParam('SNOWFLAKE_CREDENTIALS', schema_config.get('SNOWFLAKE_CREDENTIALS', allVars.get('SNOWFLAKE_CREDENTIALS')), 'The path to the Snowflake credentials file.')
-                    stringParam('WAREHOUSE', schema_config.get('WAREHOUSE', allVars.get('WAREHOUSE')), '')
-                    stringParam('ROLE', schema_config.get('ROLE', allVars.get('ROLE')))
-                    stringParam('DATABASE', schema_config.get('DATABASE', allVars.get('DATABASE')))
-                    stringParam('SCHEMA', schema_config.get('SCHEMA', allVars.get('SCHEMA')), 'Schema')
+                    stringParam('GCP_CREDENTIALS', schema_config.get('GCP_CREDENTIALS', allVars.get('GCP_CREDENTIALS')), 'The path to the GCP credentials file.')
                     stringParam('VERTICA_SCHEMA_NAME', schema_config.get('VERTICA_SCHEMA_NAME', allVars.get('VERTICA_SCHEMA_NAME')), 'Vertica Schema')
                     stringParam('VERTICA_CREDENTIALS', schema_config.get('VERTICA_CREDENTIALS', allVars.get('VERTICA_CREDENTIALS')), 'The path to the Vertica credentials file.')
-                    stringParam('VERTICA_WAREHOUSE_NAME', schema_config.get('VERTICA_WAREHOUSE_NAME', allVars.get('VERTICA_WAREHOUSE_NAME')), '')
                     stringParam('EXCLUDE', schema_config.get('EXCLUDE', allVars.get('EXCLUDE')), 'as an example: --exclude [\"f_user_activity\",\"d_user_course_certificate\",\"d_user_course\"]')
-                    
                 }
                 multiscm common_multiscm(allVars)
                 wrappers common_wrappers(allVars)
                 publishers common_publishers(allVars)
                 steps {
-                    shell(dslFactory.readFileFromWorkspace('dataeng/resources/load-vertica-schema-to-snowflake.sh'))
+                    shell(dslFactory.readFileFromWorkspace('dataeng/resources/load-vertica-schema-to-bigquery.sh'))
                 }
             }
         }
--- dataeng/jobs/analytics/VerticaSchemaToS3.groovy
+++ dataeng/jobs/analytics/VerticaSchemaToS3.groovy
@@ -8,17 +8,16 @@ import static org.edx.jenkins.dsl.AnalyticsConstants.common_wrappers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_triggers
 
-class VerticaToBigquerySchemaCopy {
+class VerticaSchemaToS3 {
     public static def job = { dslFactory, allVars ->
         allVars.get('VERTICA_SCHEMAS').each { schema, schema_config ->
-            dslFactory.job("vertica-to-bigquery-schema-copy-$schema") {
+            dslFactory.job("vertica-schema-to-s3-$schema") {
                 logRotator common_log_rotator(allVars, schema_config)
                 parameters common_parameters(allVars, schema_config)
                 parameters {
                     stringParam('SCHEMA', schema_config.get('SCHEMA', allVars.get('SCHEMA')), 'Vertica Schema')
                     stringParam('OVERWRITE', schema_config.get('OVERWRITE', allVars.get('OVERWRITE')), 'Set to: --overwrite if you want to enable overwrite.')
                     stringParam('VERTICA_CREDENTIALS', schema_config.get('VERTICA_CREDENTIALS', allVars.get('VERTICA_CREDENTIALS')), 'The path to the Vertica credentials file.')
-                    stringParam('GCP_CREDENTIALS', schema_config.get('GCP_CREDENTIALS', allVars.get('GCP_CREDENTIALS')), 'The path to the GCP credentials file.')
                     stringParam('EXCLUDE', schema_config.get('EXCLUDE', allVars.get('EXCLUDE')), 'as an example: --exclude [\"f_user_activity\",\"d_user_course_certificate\",\"d_user_course\"]')
                     stringParam('RUN_DATE', schema_config.get('RUN_DATE', allVars.get('RUN_DATE', 'today')), 'Run date for the job. A string that can be parsed by the GNU coreutils "date" utility.')
                 }
@@ -28,7 +27,7 @@ class VerticaToBigquerySchemaCopy {
                 publishers common_publishers(allVars)
                 publishers {
                     downstreamParameterized {
-                        trigger("load-vertica-$schema-schema-to-snowflake") {
+                        trigger("load-vertica-schema-to-snowflake-$schema,load-vertica-schema-to-bigquery-$schema") {
                             condition('SUCCESS')
                             parameters {
                                 // The contents of this file are generated as part of the script in the build step.
@@ -38,7 +37,7 @@ class VerticaToBigquerySchemaCopy {
                     }
                 }
                 steps {
-                    shell(dslFactory.readFileFromWorkspace('dataeng/resources/vertica-to-bigquery-schema-copy.sh'))
+                    shell(dslFactory.readFileFromWorkspace('dataeng/resources/vertica-schema-to-s3.sh'))
                 }
             }
         }
