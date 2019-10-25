ecc3415bfbc361003bd3f74387743273eec538b9
Stuart Young <syoung@edx.org>
2019-10-25 17:48:06 -0400

    fixup

--- dataeng/resources/warehouse-transforms.sh
+++ dataeng/resources/warehouse-transforms.sh
@@ -7,7 +7,7 @@ cd $WORKSPACE/warehouse-transforms
 pip install -r tools/dbt_schema_builder/requirements.txt
 
 
-cd $WORKSPACE/warehouse-transforms/app_views_project
+cd $WORKSPACE/warehouse-transforms/warehouse_transforms_project
 dbt clean
 
 dbt run --models tag:$MODEL_TAG --project $DBT_PROJECT --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
