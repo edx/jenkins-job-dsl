c280c8d9e5597c3f724127a1c12c95b7e36ecd74
Troy Sankey <tsankey@edx.org>
2020-01-15 17:13:45 -0500

    Add missing parameters to Snowflake Spreadsheet loading job.
    DE-1919

--- dataeng/resources/load-google-spreadsheet-snowflake.sh
+++ dataeng/resources/load-google-spreadsheet-snowflake.sh
@@ -4,5 +4,10 @@ env
 ${WORKSPACE}/analytics-configuration/automation/run-automated-task.sh \
  LoadGoogleSpreadsheetsToSnowflakeWorkflow --local-scheduler \
  --date $(date +%Y-%m-%d -d "$TO_DATE") \
+ --sf-credentials $SNOWFLAKE_CREDENTIALS \
+ --sf-warehouse $SNOWFLAKE_WAREHOUSE \
+ --sf-role $SNOWFLAKE_ROLE \
+ --sf-database $SNOWFLAKE_DATABASE \
+ --sf-run-id $BUILD_ID \
  --google-credentials $GOOGLE_CREDENTIALS \
  --overwrite
