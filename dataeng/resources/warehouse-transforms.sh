f6486b05d17d4fc725f711878e617ad05c1a633c
Stu Young <estute@users.noreply.github.com>
2020-01-16 15:50:42 -0500

    download dbt dependencies in warehouse transforms script (#877)

--- dataeng/resources/warehouse-transforms.sh
+++ dataeng/resources/warehouse-transforms.sh
@@ -10,6 +10,7 @@ pip install -r tools/dbt_schema_builder/requirements.txt
 cd $WORKSPACE/warehouse-transforms/warehouse_transforms_project
 
 dbt clean
+dbt deps
 dbt seed --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
 dbt run --models tag:$MODEL_TAG --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
 dbt test --models tag:$MODEL_TAG --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
