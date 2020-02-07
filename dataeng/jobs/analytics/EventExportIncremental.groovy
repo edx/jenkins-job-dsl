0d99e8e887c7f48f3c9a31c91d4d2b42d146ec56
Troy Sankey <tsankey@edx.org>
2020-02-07 12:20:03 -0500

    Add common_authorization to event export incremental job

--- dataeng/jobs/analytics/EventExportIncremental.groovy
+++ dataeng/jobs/analytics/EventExportIncremental.groovy
@@ -9,11 +9,13 @@ import static org.edx.jenkins.dsl.AnalyticsConstants.common_log_rotator
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_wrappers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_triggers
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_authorization
 
 class EventExportIncremental {
     public static def job = { dslFactory, allVars ->
         allVars.get('ENVIRONMENTS').each { environment, env_config ->
             dslFactory.job("event-export-incremental-$environment") {
+                authorization common_authorization(allVars)
                 logRotator common_log_rotator(allVars, env_config)
                 parameters common_parameters(allVars, env_config)
                 parameters from_date_interval_parameter(allVars)
