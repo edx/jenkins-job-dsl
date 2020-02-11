d544ba56365bd1c7d6fb5fd90fc9020963579849
Brian Mesick <bmesick@edx.org>
2020-02-11 15:12:30 -0500

    Add common auth to ITK Snowflake job

--- dataeng/jobs/analytics/SnowflakeMicrobachelorsITK.groovy
+++ dataeng/jobs/analytics/SnowflakeMicrobachelorsITK.groovy
@@ -10,12 +10,15 @@ import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_triggers
 import static org.edx.jenkins.dsl.AnalyticsConstants.secure_scm
 import static org.edx.jenkins.dsl.AnalyticsConstants.secure_scm_parameters
+import static org.edx.jenkins.dsl.AnalyticsConstants.common_authorization
+
 
 class SnowflakeMicrobachelorsITK {
 
     public static def job = { dslFactory, allVars ->
         dslFactory.job('snowflake-microbachelors-send-coaching-data-itk') {
             logRotator common_log_rotator(allVars)
+            authorization common_authorization(allVars)
             parameters secure_scm_parameters(allVars)
             parameters {
                 stringParam('ANALYTICS_TOOLS_URL', allVars.get('ANALYTICS_TOOLS_URL'), 'URL for the analytics tools repo.')
