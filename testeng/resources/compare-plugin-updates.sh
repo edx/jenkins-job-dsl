8d1d08139eeb2d4c501c588450d19868032c36e5
Stuart Young <syoung@edx.org>
2020-02-13 12:01:15 -0500

    fixup

--- testeng/resources/compare-plugin-updates.sh
+++ testeng/resources/compare-plugin-updates.sh
@@ -5,12 +5,12 @@ cd jenkins-configuration
 source local_env.sh.sample
 
 export PLUGIN_OUTPUT_FILE='base-installed-plugins'
-export PLUGIN_CONFIG=${BASE_CONFIG_PLUGIN_FILE}
+export PLUGIN_CONFIG=../${BASE_CONFIG_PLUGIN_FILE}
 export PLUGIN_CONFIG_KEY=${BASE_CONFIG_PLUGIN_KEY}
 make clean plugins show
 
 export PLUGIN_OUTPUT_FILE='new-installed-plugins'
-export PLUGIN_CONFIG=${TARGET_CONFIG_PLUGIN_FILE}
+export PLUGIN_CONFIG=../${TARGET_CONFIG_PLUGIN_FILE}
 export PLUGIN_CONFIG_KEY=${TARGET_CONFIG_PLUGIN_KEY}
 make clean plugins show
 
