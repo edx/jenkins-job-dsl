a3ae1aabe249061bd0e0dc98db01dd3884d5e8b5
Brian Mesick <bmesick@edx.org>
2020-02-25 15:28:38 -0500

    Separate out source tests from tag tests

--- dataeng/resources/warehouse-transforms.sh
+++ dataeng/resources/warehouse-transforms.sh
@@ -12,5 +12,10 @@ cd $WORKSPACE/warehouse-transforms/warehouse_transforms_project
 dbt clean
 dbt deps
 dbt seed --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
+
+# Run the source tests first, sadly not just the ones upstream from this tag
+dbt test --models source:* --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
 dbt run --models tag:$MODEL_TAG --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
-dbt test --models tag:$MODEL_TAG --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
+
+# Run the tests for this tag, but exclude sources since we just tested them
+dbt test --models tag:$MODEL_TAG --exclude source:* --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
