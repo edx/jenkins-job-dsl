f4807dae813c72d8bb2bce6016d8ef8cdfc2e273
Brian Mesick <bmesick@edx.org>
2020-03-24 15:18:41 -0400

    Update jobs for re-org'd warehouse-transforms

--- dataeng/resources/dbt-docs.sh
+++ dataeng/resources/dbt-docs.sh
@@ -7,15 +7,15 @@ cd $WORKSPACE/warehouse-transforms
 # To install right version of dbt
 pip install -r tools/dbt_schema_builder/requirements.txt
 
-cd $WORKSPACE/warehouse-transforms/warehouse_transforms_project
+cd $WORKSPACE/warehouse-transforms/projects/reporting
 
 dbt clean
 dbt deps
 
-# Builds the doc files to warehouse_transforms_project/target
+# Builds the doc files to projects/reporting/target
 dbt docs generate --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/ --profile $DBT_PROFILE --target $DBT_TARGET
 
 # Upload the buld docs to S3
 pip install awscli
 
-aws s3 cp --recursive ${WORKSPACE}/warehouse-transforms/warehouse_transforms_project/target/ s3://edx-dbt-docs/
+aws s3 cp --recursive ${WORKSPACE}/warehouse-transforms/projects/reporting/target/ s3://edx-dbt-docs/
