39d39a42e39a102b21039b82a547c9e71f2ca2c8
Joseph Mulloy <jmulloy@edx.org>
2020-03-24 17:01:23 -0400

    Clarify output

--- devops/resources/recover_tracking_logs.sh
+++ devops/resources/recover_tracking_logs.sh
@@ -49,7 +49,7 @@ for SNAPSHOT in ${SNAPSHOTS}; do
     echo "Checking Snapshot-ID:${SNAP_ID} Instance:${INSTANCE_ID} IP:${IP} Date:${DATE}"
     # Redirect to /dev/null because grep -q causes aws cli to complain about a broken pipe when grep stops early
     if aws s3 ls s3://${BUCKET}/logs/tracking/${SG}/${INSTANCE_ID}-${IP}/tracking.log | grep ' 0 tracking.log' > /dev/null; then
-        echo "Synced Snapshot-ID:${SNAP_ID} Instance:${INSTANCE_ID} IP:${IP} Date:${DATE}"
+        echo "Already Synced Snapshot-ID:${SNAP_ID} Instance:${INSTANCE_ID} IP:${IP} Date:${DATE}"
     else
         echo "Not Synced Snapshot-ID:${SNAP_ID} Instance:${INSTANCE_ID} IP:${IP} Date:${DATE}"
         echo "Recovering tracking logs from Snapshot ID:${SNAP_ID} Instance:${INSTANCE_ID} IP:${IP} From:${DATE}"
