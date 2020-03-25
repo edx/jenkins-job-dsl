dc3f2339b881447d84063dd34c0fffa2b77d532a
Joseph Mulloy <jmulloy@edx.org>
2020-03-25 14:13:05 -0400

    Add missing variable export

--- testeng/resources/build-packer-ami.sh
+++ testeng/resources/build-packer-ami.sh
@@ -3,12 +3,12 @@
 # This script will run packer to create an AMI for use as a Jenkins worker
 
 if [ -z "${JENKINS_WORKER_AMI}" ]; then
-JENKINS_WORKER_AMI=$(aws ec2 describe-images \
-                     --region us-east-1 \
-                     --owners 099720109477 \
-                     --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64*' \
-                     --query 'sort_by(Images,&CreationDate)[-1].ImageId' \
-                     --output text)
+export JENKINS_WORKER_AMI=$(aws ec2 describe-images \
+    --region us-east-1 \
+    --owners 099720109477 \
+    --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64*' \
+    --query 'sort_by(Images,&CreationDate)[-1].ImageId' \
+    --output text)
 fi
 
 export TEST_PLATFORM_VERSION=${PLATFORM_VERSION}
