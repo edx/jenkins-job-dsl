aed4efac0b8ce70b9f7d4ccccb5efa5d5450ac29
Julia Eskew <jeskew@edx.org>
2020-04-02 13:42:08 -0400

    Fix bash script by using proper comparison operator.

--- dataeng/resources/warehouse-transforms.sh
+++ dataeng/resources/warehouse-transforms.sh
@@ -14,7 +14,7 @@ dbt deps
 dbt seed --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
 
 # Source testing *before* model-building can be enabled/disabled with this envvar.
-if [ $TEST_SOURCES_FIRST -eq 'true' ]
+if [ $TEST_SOURCES_FIRST = 'true' ]
 then
     # Run the source tests, sadly not just the ones upstream from this tag
     dbt test --models source:* --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
@@ -23,7 +23,7 @@ fi
 # Compile/build all models with this tag.
 dbt run --models tag:$MODEL_TAG --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
 
-if [ $TEST_SOURCES_FIRST -eq 'true' ]
+if [ $TEST_SOURCES_FIRST = 'true' ]
 then
     # Run the tests for this tag, but exclude sources since we just tested them
     dbt test --models tag:$MODEL_TAG --exclude source:* --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
