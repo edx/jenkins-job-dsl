97dafcf414d195c67bd03d5d01ddd5a66cf94681
Stuart Young <syoung@edx.org>
2020-06-15 14:39:16 -0400

    run terminate cluster from correct directory

--- dataeng/jobs/analytics/TerminateCluster.groovy
+++ dataeng/jobs/analytics/TerminateCluster.groovy
@@ -16,7 +16,7 @@ class TerminateCluster {
             steps {
                 virtualenv {
                     nature("shell")
-                    command('EXTRA_VARS="name=$CLUSTER_NAME" make terminate.emr')
+                    command('EXTRA_VARS="name=$CLUSTER_NAME" cd edx-analytics-configuration && make terminate.emr')
                 }
             }
         }
