cb2a5c59fb360f3ed08ad3e9428ccb0421231bff
Stuart Young <syoung@edx.org>
2020-06-15 14:49:23 -0400

    fixup

--- dataeng/jobs/analytics/TerminateCluster.groovy
+++ dataeng/jobs/analytics/TerminateCluster.groovy
@@ -16,7 +16,7 @@ class TerminateCluster {
             steps {
                 virtualenv {
                     nature("shell")
-                    command('EXTRA_VARS="name=$CLUSTER_NAME" cd edx-analytics-configuration && make terminate.emr')
+                    command('EXTRA_VARS="name=$CLUSTER_NAME" cd $WORKSPACE/analytics-configuration && make terminate.emr')
                 }
             }
         }
