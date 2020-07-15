0fb295dcc836d883e8c70dbb1acd6203a8d6112d
Stu Young <estute@users.noreply.github.com>
2020-07-15 17:21:56 -0400

    Estute/update schema builder args (#1055)
    update schema builder cli args to reflect change to use config file and move schema builder job to new jenkins

--- dataeng/jobs/createJobs.groovy
+++ dataeng/jobs/createJobs.groovy
@@ -43,7 +43,6 @@ import static analytics.SnowflakeMicrobachelorsITK.job as SnowflakeMicrobachelor
 import static analytics.SnowflakePublicGrantsCleaner.job as SnowflakePublicGrantsCleanerJob
 import static analytics.SnowflakeRefreshSnowpipe.job as SnowflakeRefreshSnowpipeJob
 import static analytics.SnowflakeReplicaImportFromS3.job as SnowflakeReplicaImportFromS3Job
-import static analytics.SnowflakeSchemaBuilder.job as SnowflakeSchemaBuilderJob
 import static analytics.SqlScripts.multiple_scripts_job as SqlScriptsJob
 import static analytics.SqlScripts.single_script_job as SingleSqlScriptJob
 import static analytics.StitchSnowflakeLagMonitor.job as StitchSnowflakeLagMonitorJob
@@ -121,7 +120,6 @@ def taskMap = [
     SNOWFLAKE_PUBLIC_GRANTS_CLEANER_JOB: SnowflakePublicGrantsCleanerJob,
     SNOWFLAKE_REFRESH_SNOWPIPE_JOB: SnowflakeRefreshSnowpipeJob,
     SNOWFLAKE_REPLICA_IMPORT_FROM_S3_JOB: SnowflakeReplicaImportFromS3Job,
-    SNOWFLAKE_SCHEMA_BUILDER_JOB: SnowflakeSchemaBuilderJob,
     SQL_SCRIPTS_JOB: SqlScriptsJob,
     STITCH_SNOWFLAKE_LAG_MONITOR_JOB: StitchSnowflakeLagMonitorJob,
     TOTAL_EVENTS_DAILY_REPORT_JOB: TotalEventsDailyReportJob,
