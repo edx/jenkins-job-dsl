8207970cb0b9a238ad85bfef41003472fcdc7ef7
Troy Sankey <tsankey@edx.org>
2020-07-29 16:07:37 -0400

    Reconfigure sqoop loads to support stitch validation
    * Pass the Sqoop start time all the way down to the validation job.
    * Reduce frequency of loading into Snowflake.
    * Cut down the number of tables being loaded into Snowflake.
    * Refactor job configs so that they are closer to the code.
    
    DENG-359

--- dataeng/jobs/analytics/SnowflakeReplicaImportFromS3.groovy
+++ dataeng/jobs/analytics/SnowflakeReplicaImportFromS3.groovy
@@ -1,36 +1,91 @@
 package analytics
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_multiscm
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_parameters
-import static org.edx.jenkins.dsl.AnalyticsConstants.to_date_interval_parameter
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_log_rotator
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_wrappers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
-import static org.edx.jenkins.dsl.AnalyticsConstants.common_triggers
 
 class SnowflakeReplicaImportFromS3 {
     public static def job = { dslFactory, allVars ->
-        allVars.get('SNOWFLAKE_READ_REPLICA_IMPORTS').each { db, db_config ->
-            dslFactory.job("snowflake-${db.toLowerCase()}-read-replica-import-from-s3") {
+
+        Map jobConfigs = [
+            CREDENTIALS: [
+                CLUSTER_NAME: 'ImportCredentialsReadReplicaSnowflakeFromS3',
+                SCHEMA: 'CREDENTIALS_RAW',
+                SCRATCH_SCHEMA: 'CREDENTIALS_SCRATCH_LOADING',
+                DATABASE: 'credentials',
+                INCLUDE: '--include ["core_user", "django_session", "social_auth_usersocialauth"]',
+            ],
+            LMS: [
+                CLUSTER_NAME: 'ImportLMSReadReplicaSnowflakeFromS3',
+                SCHEMA: 'LMS_RAW',
+                SCRATCH_SCHEMA: 'LMS_SCRATCH_LOADING',
+                DATABASE: 'wwc',
+                INCLUDE: '--include ["assessment_assessment", "assessment_assessmentfeedback", "assessment_assessmentpart", "auth_userprofile", "certificates_generatedcertificate", "course_goals_coursegoal", "course_overviews_courseoverviewtab", "edxval_videoimage", "enterprise_enterprisecustomer", "enterprise_historicalenterprisecustomer", "entitlements_courseentitlement", "problem_builder_answer", "proctoring_proctoredexamstudentattempt", "social_auth_usersocialauth", "student_courseenrollment", "student_loginfailures", "student_sociallink", "submissions_scoresummary", "user_api_usercoursetag", "wiki_article", "wiki_articlerevision", "workflow_assessmentworkflow"]',
+            ],
+            DISCOVERY: [
+                CLUSTER_NAME: 'ImportDiscoveryReadReplicaSnowflakeFromS3',
+                SCHEMA: 'DISCOVERY_RAW',
+                SCRATCH_SCHEMA: 'DISCOVERY_SCRATCH_LOADING',
+                DATABASE: 'discovery',
+                INCLUDE: '--include ["catalogs_catalog"]',
+            ],
+            ECOMMERCE: [
+                CLUSTER_NAME: 'ImportEcommerceReadReplicaSnowflakeFromS3',
+                SCHEMA: 'ECOMMERCE_RAW',
+                SCRATCH_SCHEMA: 'ECOMMERCE_SCRATCH_LOADING',
+                DATABASE: 'ecommerce',
+                INCLUDE: '--include ["basket_line", "basket_basket_vouchers", "catalogue_historicalproduct", "catalogue_product", "catalogue_productattributevalue", "django_session", "ecommerce_user", "invoice_invoice", "offer_condition", "offer_offerassignment", "offer_offerassignmentemailtemplates", "order_line", "refund_refund"]',
+            ],
+        ]
+
+        jobConfigs.each { appName, jobConfig ->
+            dslFactory.job("snowflake-${appName.toLowerCase()}-read-replica-import-from-s3") {
                 logRotator common_log_rotator(allVars)
-                parameters common_parameters(allVars, db_config)
+                parameters common_parameters(allVars, jobConfig)
                 parameters {
-                    stringParam('RUN_DATE', db_config.get('RUN_DATE', allVars.get('RUN_DATE', 'today')), 'Run date for the job. A string that can be parsed by the GNU coreutils "date" utility.')
-                    stringParam('OVERWRITE', db_config.get('OVERWRITE', allVars.get('OVERWRITE')), 'Set to: --overwrite if you want to enable overwrite.')
-                    stringParam('SNOWFLAKE_CREDENTIALS', db_config.get('SNOWFLAKE_CREDENTIALS', allVars.get('SNOWFLAKE_CREDENTIALS')), 'The path to the Snowflake credentials file.')
-                    stringParam('WAREHOUSE', db_config.get('WAREHOUSE', allVars.get('WAREHOUSE')), '')
-                    stringParam('ROLE', db_config.get('ROLE', allVars.get('ROLE')))
-                    stringParam('SNOWFLAKE_DATABASE', db_config.get('SNOWFLAKE_DATABASE', allVars.get('SNOWFLAKE_DATABASE')))
-                    stringParam('SCHEMA', db_config.get('SCHEMA', allVars.get('SCHEMA')), 'Schema')
-                    stringParam('SCRATCH_SCHEMA', db_config.get('SCRATCH_SCHEMA', allVars.get('SCRATCH_SCHEMA')), 'Scratch schema name - temporary loading location.')
-                    stringParam('DATABASE', db_config.get('DATABASE'), 'Name of MySQL database to copy from.')
-                    stringParam('INCLUDE', db_config.get('INCLUDE'), 'List of regular expressions matching table names that should be copied.')
-                    stringParam('EXCLUDE', db_config.get('EXCLUDE'), 'List of regular expressions matching table names that should not be copied.')
+                    stringParam('RUN_DATE', 'today', 'Run date for the job. A string that can be parsed by the GNU coreutils "date" utility.')
+                    stringParam('OVERWRITE', '--overwrite', 'Set to: --overwrite if you want to enable overwrite.')
+                    stringParam('SNOWFLAKE_CREDENTIALS', allVars.get('SNOWFLAKE_CREDENTIALS'), 'The path to the Snowflake credentials file.')
+                    stringParam('WAREHOUSE', 'LOADING_SQOOP', 'The Snowflake warehouse to use.')
+                    stringParam('ROLE', 'PIPELINE_ETL_LOADER_ROLE', 'The Snowflake role to assume.')
+                    stringParam('SNOWFLAKE_DATABASE', 'PROD', 'The destination database in Snowflake.')
+                    stringParam('SCHEMA', jobConfig.get('SCHEMA'), 'The destination raw schema in Snowflake.')
+                    stringParam('SCRATCH_SCHEMA', jobConfig.get('SCRATCH_SCHEMA'), 'Scratch schema name - temporary loading location in Snowflake.')
+                    stringParam('DATABASE', jobConfig.get('DATABASE'), 'Name of MySQL database to copy from.')
+                    stringParam('INCLUDE', jobConfig.get('INCLUDE', ''), 'List of regular expressions matching table names that should be copied.')
+                    stringParam('EXCLUDE', jobConfig.get('EXCLUDE', ''), 'List of regular expressions matching table names that should not be copied.')
+                    stringParam('SQOOP_START_TIME', '', 'The start time of the upstream sqoop load (in ISO timestamp format).')
+                    booleanParam('FORCE', false, 'Force the job to run today, regardless of whether it is scheduled for today.')
+                }
+                environmentVariables {
+                    env('APP_NAME', appName)
                 }
                 multiscm common_multiscm(allVars)
                 wrappers common_wrappers(allVars)
-                publishers common_publishers(allVars)
                 steps {
                     shell(dslFactory.readFileFromWorkspace('dataeng/resources/snowflake-replica-import.sh'))
+                }
+                postBuildSteps {
+                    // If we skipped today's run, set the current build to ABORTED so that we don't trigger stitch
+                    // validation.
+                    conditionalSteps {
+                        condition { fileExists('${WORKSPACE}/build_skipped') }
+                        steps { setBuildResult('ABORTED') }
+                    }
+                }
+                publishers common_publishers(allVars)
+                publishers {
+                    downstreamParameterized {
+                        trigger("snowflake-validate-stitch" )
+                        {
+                            condition('SUCCESS')
+                            parameters {
+                                // The contents of this file are generated as part of the script in the build step.
+                                propertiesFile('${WORKSPACE}/downstream.properties')
+                            }
+                        }
+                    }
                 }
             }
         }
