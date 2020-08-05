7933120a9a05f9e1f9bea0a43520a192015c0b91
Hassan Javeed <hassan@edx.org>
2020-08-06 01:15:58 +0500

    test

--- dataeng/jobs/analytics/TestHassan.groovy
+++ dataeng/jobs/analytics/TestHassan.groovy
@@ -6,15 +6,20 @@ import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
 class TestHassan {
     public static def job = { dslFactory, allVars ->
         dslFactory.job("test-hassan-param") {
-            parameters {
-                stringParam('NOTIFY', '', '')
-            }
-            binding.variables.each {
-              println "${it.key} = ${it.value}"
-            }
-            publishers {
-                if ('$NOTIFYNOTEXISTS') {
-                    mailer('$NOTIFY')
+            steps{
+                conditionalSteps {
+                  condition {
+                    dayCondition {
+                      daySelector {
+                         monday()
+                      }
+                      useBuildTime(false)
+                    }
+                  }
+                  runner('Fail')
+                  steps {
+                    shell('echo Hello')
+                  }
                 }
             }
         }
