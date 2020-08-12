d7a10478eba4601884390eb1acad4da157170ea0
Brian Mesick <bmesick@edx.org>
2020-08-12 11:39:08 -0400

    Fix issues with excluded models being run
    This ticket intended for the given model tag to not be run or tested:
    https://openedx.atlassian.net/servicedesk/customer/portal/16/DESUPPORT-414
    
    We only excluded it from tests:
    https://github.com/edx/jenkins-job-dsl/pull/1067/files
    
    This PR fixes that to also exclude from runs.

--- dataeng/resources/warehouse-transforms.sh
+++ dataeng/resources/warehouse-transforms.sh
@@ -24,31 +24,31 @@ then
     dbt test --models source:* --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
 fi
 
+# By default, exclude no models from testing.
+exclude_models=""
+exclude_param=""
+
+if [ "$MODEL_TAG" = 'daily' ]
+then
+    # For 'daily' models, exclude these models from being run or tested
+    exclude_models="$exclude_models finrep_map_organization_course_courserun finrep_royalty_order_dimension tag:daily-exclude"
+fi
+
+if [ "$exclude_models" != "" ]
+then
+    # If models were excluded, add the leading exclude parameter.
+    exclude_param="--exclude $exclude_models"
+fi
+
 # Compile/build all models with this tag.
-dbt run --models tag:$MODEL_TAG --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
+dbt run --models tag:$MODEL_TAG $exclude_param --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
 
 if [ "$SKIP_TESTS" != 'true' ]
 then
-    # By default, exclude no models from testing.
-    exclude_models=""
-    exclude_param=""
-
     if [ "$TEST_SOURCES_FIRST" = 'true' ]
     then
         # Exclude the sources from testing, since they were already tested pre-'dbt run'.
         exclude_models="$exclude_models source:*"
-    fi
-
-    if [ "$MODEL_TAG" = 'daily' ]
-    then
-        # For 'daily' models, exclude these two models from finrep that inexplicably are tested when testing those models.
-        exclude_models="$exclude_models finrep_map_organization_course_courserun finrep_royalty_order_dimension tag:daily-exclude"
-    fi
-
-    if [ "$exclude_models" != "" ]
-    then
-        # If models were excluded, add the leading exclude parameter.
-        exclude_param="--exclude $exclude_models"
     fi
 
     # Run all tests which haven't been excluded.
