0d4439c265a27446d43538f1b696a88b21828fb5
Matt Hughes <mhughes@edx.org>
2020-09-16 16:57:18 -0400

    Delete setup files for the supporting jenkins jobs in ora2 pipeline
    JIRA:EDUCATOR-5335

--- ora2/resources/run-ora2-acceptance-tests.sh
+++
@@ -1,4 +0,0 @@
-# The environment needs a time to start. TODO: Make this wait more intelligent.
-sleep ${SLEEP_TIME}
-
-xvfb-run --server-args="-screen 0 1600x1200x24" ./scripts/jenkins-acceptance-tests.sh
