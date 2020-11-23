c1359a062e823686eb29fcfd0d23258ed8ecf6c6
Tim McCormack <tmccormack@edx.org>
2020-11-23 21:16:28 +0000

    ARCHBOM-1632: Stop using deprecated --sha and --org options
    Also, use the `org` config key when cloning the repository from Github
    (avoid cross-org name collisions) and use it in the job name and
    messaging as well.
    
    After this PR, the deprecated options can be removed from the scripts
    in testeng-ci (although a better long-term option would be to call the
    PR creator CLI directly now that it exists.)

--- testeng/resources/cleanup-python-code-create-pr.sh
+++ testeng/resources/cleanup-python-code-create-pr.sh
@@ -13,7 +13,7 @@ for repo in "${REPOS[@]}"; do
 
   rm -rf "${repo}"
 
-  if git clone https://github.com/edx/"${repo}".git; then true; else failed_repos+=("${repo}") && continue; fi
+  if git clone "https://github.com/$ORG/$repo.git"; then true; else failed_repos+=("${repo}") && continue; fi
 
   rm -rf "${repo}"-code_cleanup_venv
   virtualenv --python=python"$PYTHON_VERSION" "${repo}"-code_cleanup_venv -q
@@ -22,12 +22,8 @@ for repo in "${REPOS[@]}"; do
   echo "Upgrading pip..."
   if pip install pip==20.0.2; then true; else failed_repos+=("${repo}") && continue; fi
 
-  echo "Getting current sha..."
-  cd "${repo}"
-  if git rev-parse HEAD &> /dev/null; then true; else failed_repos+=("${repo}") && continue; fi
-  export CURRENT_SHA=$(git rev-parse HEAD)
-
   echo "Running cleanup..."
+  cd "${repo}"
 
   if pip install $PACKAGESTOINSTALL; then true; else failed_repos+=("${repo}") && continue; fi
 
@@ -39,7 +35,7 @@ for repo in "${REPOS[@]}"; do
   echo "Running script to create PR..."
   cd ../testeng-ci
   if pip install -r requirements/base.txt; then true; else failed_repos+=("${repo}") && continue ; fi
-  if python -m jenkins.cleanup_python_code --sha=$CURRENT_SHA --repo_root="../${repo}" --user_reviewers=$PR_USER_REVIEWERS --team_reviewers=$PR_TEAM_REVIEWERS --packages="$PACKAGES" --scripts="$SCRIPTS"
+  if python -m jenkins.cleanup_python_code --repo_root="../${repo}" --user_reviewers=$PR_USER_REVIEWERS --team_reviewers=$PR_TEAM_REVIEWERS --packages="$PACKAGES" --scripts="$SCRIPTS"
   then
     true
   else
--- testeng/resources/upgrade-python-requirements.sh
+++ testeng/resources/upgrade-python-requirements.sh
@@ -8,17 +8,14 @@ source upgrade_venv/bin/activate
 echo "Upgrading pip..."
 pip install pip==20.0.2
 
-echo "Getting current sha..."
-cd $REPO_NAME
-export CURRENT_SHA=$(git rev-parse HEAD)
-
 echo "Running make upgrade..."
+cd $REPO_NAME
 make upgrade
 
 echo "Running script to create PR..."
 cd ../testeng-ci
 pip install -r requirements/base.txt
-python -m jenkins.upgrade_python_requirements --sha=$CURRENT_SHA --repo_root="../$REPO_NAME" --org=$ORG --user_reviewers=$PR_USER_REVIEWERS --team_reviewers=$PR_TEAM_REVIEWERS
+python -m jenkins.upgrade_python_requirements --repo_root="../$REPO_NAME" --user_reviewers=$PR_USER_REVIEWERS --team_reviewers=$PR_TEAM_REVIEWERS
 
 deactivate
 cd ..
