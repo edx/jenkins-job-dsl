0c4adaea51fdf5fa8c0d21f1e54cb835d3fb4a86
Stuart Young <syoung@edx.org>
2020-12-18 14:17:30 -0500

    remove call to disable insights pipeline jobs

--- dataeng/jobs/analytics/TotalEventsDailyReport.groovy
+++ dataeng/jobs/analytics/TotalEventsDailyReport.groovy
@@ -9,11 +9,6 @@ import static org.edx.jenkins.dsl.AnalyticsConstants.common_triggers
 class TotalEventsDailyReport {
     public static def job = { dslFactory, allVars ->
         dslFactory.job("total-events-daily-report") {
-            // As part of the MySQL upgrade for Insights/Data API, we need to disable the jobs that
-            // interface with the resultstore.
-            // TODO: once the upgrade is complete for both prod and edge environments, remove this line
-            disabled(allVars.get('JOB_DISABLED'))
-
             logRotator common_log_rotator(allVars)
             parameters common_parameters(allVars)
             parameters {
