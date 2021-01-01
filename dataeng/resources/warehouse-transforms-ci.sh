37913c4dabd026ddbb14bee7cbb9a3ded98aa2b1
Jazib Humayun <jazibhumayun@gmail.com>
2021-01-01 20:00:32 +0500

    removed seed and run commands

--- dataeng/resources/warehouse-transforms-ci.sh
+++ dataeng/resources/warehouse-transforms-ci.sh
@@ -9,12 +9,13 @@ cd $WORKSPACE/warehouse-transforms
 # To install right version of dbt
 pip install -r requirements.txt
 
-# where this WORKSPACE is defined ? # Comment to be removed
 cd $WORKSPACE/warehouse-transforms/projects/$DBT_PROJECT_PATH
 
 
 dbt clean --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/ --profile $DBT_PROFILE --target $DBT_TARGET
 dbt deps --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/ --profile $DBT_PROFILE --target $DBT_TARGET
-dbt seed --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
-dbt run $DBT_RUN_OPTIONS $DBT_RUN_EXCLUDE --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
-dbt test $DBT_RUN_OPTIONS --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
+
+# WIP: Running on only google analytics sessions models tag to speed up the test runs
+dbt test --models tag:google_analytics_sessions --exclude 'source:*' --profile warehouse_transforms --target prod --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
+#dbt test $DBT_RUN_OPTIONS --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
+
