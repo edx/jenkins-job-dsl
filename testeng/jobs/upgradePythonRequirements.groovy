20ff917c2b002ea85bf7f91b5137dc7525653022
M. Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2021-01-15 14:36:11 +0500

    Upgrade edx-rbac to Python 3.8 (#1263)

--- testeng/jobs/upgradePythonRequirements.groovy
+++ testeng/jobs/upgradePythonRequirements.groovy
@@ -366,7 +366,7 @@ List jobConfigs = [
         org: 'edx',
         repoName: 'edx-rbac',
         defaultBranch: 'master',
-        pythonVersion: '3.5',
+        pythonVersion: '3.8',
         cronValue: cronOffHoursBusinessWeekdayLahore,
         githubUserReviewers: [],
         githubTeamReviewers: ['arbi-bom'],
