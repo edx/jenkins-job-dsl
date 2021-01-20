ed19371967a3e2dcae098c529a935fa0eff98125
Brian Mesick <bmesick@edx.org>
2021-01-20 11:03:15 -0500

    Unlink Vertica BI, finance, and production jobs (#1270)

--- dataeng/jobs/createJobs.groovy
+++ dataeng/jobs/createJobs.groovy
@@ -12,18 +12,12 @@ import static analytics.EnrollmentValidationEvents.job as EnrollmentValidationEv
 import static analytics.Enterprise.job as EnterpriseJob
 import static analytics.EventExportIncremental.job as EventExportIncrementalJob
 import static analytics.EventExportIncrementalLarge.job as EventExportIncrementalLargeJob
-import static analytics.FinanceReport.cybersource_pull_job as CybersourcePullJob
-import static analytics.FinanceReport.finance_report_job as FinanceReportJob
-import static analytics.FinanceReport.payments_validation_job as PaymentsValidationJob
 import static analytics.JenkinsBackup.job as JenkinsBackupJob
-import static analytics.LoadAffiliateWindowToWarehouse.job as LoadAffiliateWindowWarehouseJob
 import static analytics.LoadEvents.load_events_to_s3_job as LoadEventsToS3Job
 import static analytics.LoadEvents.load_events_to_vertica_job as LoadEventsToVerticaJob
 import static analytics.LoadGoogleAnalyticsPermissions.job as LoadGoogleAnalyticsPermissionsJob
 import static analytics.LoadGoogleSpreadsheetToSnowflake.job as LoadGoogleSpreadsheetToSnowflakeJob
 import static analytics.LoadInsightsToVertica.job as LoadInsightsToVerticaJob
-import static analytics.LoadPaypalCaseReportToVertica.job as PayPalCaseReportLoadJob
-import static analytics.LoadWarehouse.vertica_job as LoadWarehouseVerticaJob
 import static analytics.ModuleEngagement.job as ModuleEngagementJob
 import static analytics.ReadReplicaExportToS3.job as ReadReplicaExportToS3Job
 import static analytics.SnowflakeDemographicsCleanup.job as SnowflakeDemographicsCleanupJob
@@ -32,8 +26,6 @@ import static analytics.SnowflakePublicGrantsCleaner.job as SnowflakePublicGrant
 import static analytics.SnowflakeRefreshSnowpipe.job as SnowflakeRefreshSnowpipeJob
 import static analytics.SnowflakeReplicaImportFromS3.job as SnowflakeReplicaImportFromS3Job
 import static analytics.SnowflakeValidateStitch.job as SnowflakeValidateStitchJob
-import static analytics.SqlScripts.multiple_scripts_job as SqlScriptsJob
-import static analytics.SqlScripts.single_script_job as SingleSqlScriptJob
 import static analytics.StitchSnowflakeLagMonitor.job as StitchSnowflakeLagMonitorJob
 import static analytics.UserActivity.job as UserActivityJob
 import static analytics.UserLocationByCourse.job as UserLocationByCourseJob
@@ -66,7 +58,6 @@ def taskMap = [
     ANALYTICS_EXPORTER_JOB: AnalyticsExporterJob,
     ANSWER_DISTRIBUTION_JOB: AnswerDistributionJob,
     BACKUP_VERTICA_JOB: BackupVerticaJob,
-    CYBERSOURCE_PULL_JOB: CybersourcePullJob,
     DATABASE_EXPORT_COURSEWARE_STUDENTMODULE_JOB: DatabaseExportCoursewareStudentmoduleJob,
     DBT_DOCS_JOB: DBTDocsJob,
     DBT_MANUAL_JOB: DBTManualJob,
@@ -76,27 +67,20 @@ def taskMap = [
     ENTERPRISE_JOB: EnterpriseJob,
     EVENT_EXPORT_INCREMENTAL_JOB: EventExportIncrementalJob,
     EVENT_EXPORT_INCREMENTAL_LARGE_JOB: EventExportIncrementalLargeJob,
-    FINANCE_REPORT_JOB: FinanceReportJob,
     JENKINS_BACKUP_JOB: JenkinsBackupJob,
-    LOAD_AFFILIATE_WINDOW_JOB: LoadAffiliateWindowWarehouseJob,
     LOAD_EVENTS_TO_S3_JOB: LoadEventsToS3Job,
     LOAD_EVENTS_TO_VERTICA_JOB: LoadEventsToVerticaJob,
     LOAD_GOOGLE_ANALYTICS_PERMISSIONS_JOB: LoadGoogleAnalyticsPermissionsJob,
     LOAD_GOOGLE_SPREADSHEET_TO_SNOWFLAKE_JOB: LoadGoogleSpreadsheetToSnowflakeJob,
     LOAD_INSIGHTS_TO_VERTICA_JOB: LoadInsightsToVerticaJob,
-    LOAD_PAYPAL_CASEREPORT_TO_VERTICA_JOB: PayPalCaseReportLoadJob,
-    LOAD_WAREHOUSE_VERTICA_JOB: LoadWarehouseVerticaJob,
     MODULE_ENGAGEMENT_JOB: ModuleEngagementJob,
-    PAYMENTS_VALIDATION_JOB: PaymentsValidationJob,
     READ_REPLICA_EXPORT_TO_S3_JOB: ReadReplicaExportToS3Job,
-    SINGLE_SQL_SCRIPT_JOB: SingleSqlScriptJob,
     SNOWFLAKE_DEMOGRAPHICS_CLEANUP_JOB: SnowflakeDemographicsCleanupJob,
     SNOWFLAKE_MICROBACHELORS_ITK_JOB: SnowflakeMicrobachelorsITKJob,
     SNOWFLAKE_PUBLIC_GRANTS_CLEANER_JOB: SnowflakePublicGrantsCleanerJob,
     SNOWFLAKE_REFRESH_SNOWPIPE_JOB: SnowflakeRefreshSnowpipeJob,
     SNOWFLAKE_REPLICA_IMPORT_FROM_S3_JOB: SnowflakeReplicaImportFromS3Job,
     SNOWFLAKE_VALIDATE_STITCH_JOB: SnowflakeValidateStitchJob,
-    SQL_SCRIPTS_JOB: SqlScriptsJob,
     STITCH_SNOWFLAKE_LAG_MONITOR_JOB: StitchSnowflakeLagMonitorJob,
     USER_ACTIVITY_JOB: UserActivityJob,
     USER_LOCATION_BY_COURSE_JOB: UserLocationByCourseJob,
@@ -155,12 +139,9 @@ listView('Exporter') {
 
 listView('Warehouse') {
     jobs {
-        name('finance-report')
-        name('payments-validation')
-        name('affiliate-window')
         name('snowflake-schema-builder')
         regex('refresh-snowpipe-.*')
-        regex('.+read-replica-(import|export)-(to|from)-s3|load-.+|vertica-schema-to.+|.*sql-script.*')
+        regex('.+read-replica-(import|export)-(to|from)-s3|load-.+|vertica-schema-to.+')
     }
     columns DEFAULT_VIEW.call()
 }
