73eb48817fed480a4529b485bfa7f1d6f0133c9e
Jazib Humayun <jazibhumayun@gmail.com>
2021-01-22 23:37:01 +0500

    w-t-ci jenkins job

--- dataeng/resources/warehouse-transforms-ci.sh
+++ dataeng/resources/warehouse-transforms-ci.sh
@@ -35,12 +35,12 @@ pip install -r requirements.txt
 
 cd $WORKSPACE/warehouse-transforms/projects/$DBT_PROJECT_PATH
 
-# Give target CI_TESTS
 dbt clean --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/ --profile $DBT_PROFILE --target $DBT_TARGET
 dbt deps --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/ --profile $DBT_PROFILE --target $DBT_TARGET
 dbt seed --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/ --profile $DBT_PROFILE --target $DBT_TARGET
 
-
+# Default DBT_RUN_OPTIONS are '-m state:modified+ --defer --state $WORKSPACE/manifest' 
+# This is a Slim CI syntax used to "run" only modified and downstream models
 dbt run $DBT_RUN_OPTIONS $DBT_RUN_EXCLUDE --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/ --profile $DBT_PROFILE --target $DBT_TARGET
 
 dbt test $DBT_TEST_OPTIONS --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/ --profile $DBT_PROFILE --target $DBT_TARGET
