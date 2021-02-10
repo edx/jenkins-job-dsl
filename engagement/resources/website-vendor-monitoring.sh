f5cc213a0544f68cf903722dd70988d223ba01ab
albemarle <45690905+albemarle@users.noreply.github.com>
2021-02-10 11:34:43 -0500

    WS-1086: job to monitor Website vendors

---
+++ engagement/resources/website-vendor-monitoring.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -exuo pipefail
+
+set +u
+. /edx/var/jenkins/jobvenvs/virtualenv_tools.sh
+# creates a venv with its location stored in variable "venvpath"
+create_virtualenv --python=python3.8 --clear
+. "$venvpath/bin/activate"
+set -u
+
+cd $WORKSPACE/prospectus
+#pip install -r util/jenkins/requirements-cloudflare.txt
+#env
+
+python scripts/vendor-monitoring.py\
+    --algolia_auth_key ${ALGOLIA_AUTH_KEY}\
+    --contentful_auth_key ${CONTENTFUL_AUTH_KEY}\
+    --email ${EMAIL}
