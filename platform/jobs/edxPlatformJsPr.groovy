d7563f3825c327aebebb1828ecf81e22e02f3d33
Christopher Pappas <cpappas@edx.org>
2021-02-11 12:33:58 -0500

    Remove stale branches during job runs

--- platform/jobs/edxPlatformJsPr.groovy
+++ platform/jobs/edxPlatformJsPr.groovy
@@ -165,6 +165,8 @@ jobConfigs.each { jobConfig ->
                         timeout(10)
                     }
                     cleanBeforeCheckout()
+                    pruneBranches()
+                    pruneStaleBranch()
                 }
             }
         }
