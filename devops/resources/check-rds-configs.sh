7a93e39cd5ef9c63f266d8ce22fd41b53930af7e
nadeemshahzad <nadem.shahzad@gmail.com>
2021-02-25 18:06:03 +0500

    fix path

--- devops/resources/check-rds-configs.sh
+++ devops/resources/check-rds-configs.sh
@@ -14,7 +14,7 @@ cd $WORKSPACE/configuration
 . util/jenkins/assume-role.sh
 assume-role ${ROLE_ARN}
 
-cd $WORKSPACE/configuration/util/check_rds_slow_query_logs
+cd $WORKSPACE/configuration/util/check_rds_configs
 
 pip install -r requirements.txt
 
