335ae1d350cf0d618e564cf1716b328458f43631
usamasadiq <usama.sadiq@arbisoft.com>
2021-03-01 22:41:15 +0500

    build: bump the oep2-report job to python3.8

--- testeng/resources/create-oep-report.sh
+++ testeng/resources/create-oep-report.sh
@@ -1,3 +1,5 @@
+virtualenv --python=python3.8 --clear oep-venv
+. oep-venv/bin/activate
 pip install -r requirements.txt
 pip install .
 oep2 report --username edx-status-bot -- --org edx --junit-xml=oep2-report.xml
