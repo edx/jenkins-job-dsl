1ad54a4b4de6b27d4bb670f20e2976ebddb23e33
Jazib Humayun <jazibhumayun@gmail.com>
2021-04-01 12:27:36 +0500

    feat:re-run logic, test more than once incase of failure
    Sometimes WT CI jobs fail due to data being changed during tests. We have created a script to rerun flaky test and this PR consumes that script inorder to execute tests multiple times before marking a build as failed  Jira: https://openedx.atlassian.net/browse/DENG-812

--- dataeng/jobs/analytics/WarehouseTransformsCI.groovy
+++ dataeng/jobs/analytics/WarehouseTransformsCI.groovy
@@ -37,6 +37,8 @@ class WarehouseTransformsCI{
                 env('PASSPHRASE_PATH', allVars.get('PASSPHRASE_PATH'))
                 env('USER', allVars.get('USER'))
                 env('ACCOUNT', allVars.get('ACCOUNT'))
+                env('WITH_RETRY', allVars.get('WITH_RETRY'))
+                env('NO_OF_TRIES', allVars.get('NO_OF_TRIES'))
             }
             scm {
                  github('edx/warehouse-transforms')
