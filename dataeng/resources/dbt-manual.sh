06d15e2addbd0dc9a85bc21749bdfecf5588e5bd
Stuart Young <syoung@edx.org>
2021-04-09 14:18:51 -0400

    fix: add full-refresh flag to dbt seed commands in wt jobs

--- dataeng/resources/dbt-manual.sh
+++ dataeng/resources/dbt-manual.sh
@@ -11,6 +11,6 @@ cd $WORKSPACE/warehouse-transforms/projects/$DBT_PROJECT_PATH
 
 dbt clean --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/ --profile $DBT_PROFILE --target $DBT_TARGET
 dbt deps --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/ --profile $DBT_PROFILE --target $DBT_TARGET
-dbt seed --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
+dbt seed --full-refresh --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
 dbt run $DBT_RUN_OPTIONS $DBT_RUN_EXCLUDE --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
 dbt test $DBT_RUN_OPTIONS --profile $DBT_PROFILE --target $DBT_TARGET --profiles-dir $WORKSPACE/analytics-secure/warehouse-transforms/
