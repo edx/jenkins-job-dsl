71d13670a5e2f057e9ef3d6da2cb0dff71139578
Jazib Humayun <jazibhumayun@gmail.com>
2021-04-30 21:47:26 +0500

    updates

--- dataeng/resources/prefect-flows-deployment.sh
+++ dataeng/resources/prefect-flows-deployment.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
+PLATFORM_VENV="platform_venv"
+virtualenv --python=python3.8 --clear "${PLATFORM_VENV}"
+source "${PLATFORM_VENV}/bin/activate"
 
 # Setup to run dbt commands
 cd $WORKSPACE/prefect-flows
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 216367352155.dkr.ecr.us-east-1.amazonaws.com
+#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 216367352155.dkr.ecr.us-east-1.amazonaws.com
 pip install prefect
 prefect auth login -t $PREFECT_API_TOKEN
 
