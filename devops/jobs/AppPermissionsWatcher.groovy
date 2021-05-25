5777f7d88b1749585b2ba8515e86c698c790ed81
Kyle McCormick <kmccormick@edx.org>
2021-05-25 13:59:53 -0400

    fix: update AppPermissionsWatcher with new job types (#1385)
    In AppPermissionsRunner, the 'groups' job type was split into
    'groups-lms' and 'groups-cms'. This commit fixes
    AppPermissionsWatcher (which triggers the AppPermissionRunner
    jobs) to use the new job types as well.
    
    TNL-8274

--- devops/jobs/AppPermissionsWatcher.groovy
+++ devops/jobs/AppPermissionsWatcher.groovy
@@ -81,7 +81,7 @@ class AppPermissionsWatcher{
 
             triggers merge_to_master_trigger(repo_branch)
 
-            List jobTypes = ['groups', 'active-users', 'inactive-users']
+            List jobTypes = ['groups-lms', 'groups-cms', 'active-users', 'inactive-users']
             steps{
                 extraVars.get('DEPLOYMENTS').each { deployment, configuration ->
                     configuration.environments.each { environment ->
