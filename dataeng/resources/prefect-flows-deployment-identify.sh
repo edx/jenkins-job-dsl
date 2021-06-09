b0408acac26cc869042578cc0d6b31b894fdfadf
Jazib Humayun <jazibhumayun@gmail.com>
2021-06-09 17:31:03 +0500

    identification and removing of duplicates simplified

--- dataeng/resources/prefect-flows-deployment-identify.sh
+++ dataeng/resources/prefect-flows-deployment-identify.sh
@@ -5,53 +5,39 @@ set -ex
 # Setup to run dbt commands
 cd $WORKSPACE/prefect-flows
 
-# Only run deployment on merge commits, otherwise exit
-# HEAD_COMMIT=$(git rev-parse HEAD)
-# LAST_MERGE_COMMIT=$(git log --merges origin/master --format='%H' --max-count=1)
-# if [ $HEAD_COMMIT == $LAST_MERGE_COMMIT ]
-# then
-#     echo "This is one of merge commit, Run CD"
-# else
-#     echo "Exiting because not a merge commit"
-#     exit 0
-# fi
+#Only run deployment on merge commits, otherwise exit
+HEAD_COMMIT=$(git rev-parse HEAD)
+LAST_MERGE_COMMIT=$(git log --merges origin/master --format='%H' --max-count=1)
+if [ $HEAD_COMMIT == $LAST_MERGE_COMMIT ]
+then
+    echo "This is one of merge commit, Run CD"
+else
+    echo "Exiting because not a merge commit"
+    exit 0
+fi
 
 
-# Get second last merge commit id and compares it with the HEAD to find (git diff) files changed.
-#PREV_MERGE_COMMIT_ID=$(git log --merges origin/master --format='%H' --max-count=2 | sed -n 2p)
-PREV_MERGE_COMMIT_ID=11649e9390caea73ea254bcd22f58de769ecd1b8
+#Get second last merge commit id and compares it with the HEAD to find (git diff) files changed.
+PREV_MERGE_COMMIT_ID=$(git log --merges origin/master --format='%H' --max-count=2 | sed -n 2p)
+
 git diff $PREV_MERGE_COMMIT_ID --name-only # Printing for debug purpose
 
-# Search for .py and .toml extension files in the git diff
-FULLFILE=$(git diff $PREV_MERGE_COMMIT_ID --name-only | grep -E ".*\.(py|toml)")
 
 # Remove downstream.properties file to write new input
 rm -f ${WORKSPACE}/downstream.properties
 # Output 'FLOW_NAME=' in file. This will be used as name of parameter in downstream jobs
 echo -n "FLOW_NAME=" > "${WORKSPACE}/downstream.properties"
 
-# Loop will extract the immediate parent directory name of all the .toml or .py files that has changed
-files=($FULLFILE)
-for file in "${files[@]}"
-do
-  # Taking the immediate parent name e.g.if flows/load_google_sheets_to_snowflake/load_google_sheets_to_snowflake.py is the modified file it will extract the immediate
-  # parent directory name which is load_google_sheets_to_snowflake
-  parentname="$(basename "$(dirname "$file")")"
-  temp="$(echo -n "${parentname} ")"
-  list=$list$temp
-done
+# Extract the immediate parent directory name of all the .toml or .py files those are changed and removing the duplicates that
+# may occur if both .toml and .py files of a flow has changed.
+FLOWS_TO_DEPLOY=$(git diff $PREV_MERGE_COMMIT_ID --name-only | grep -E ".*\.(py|toml)" |cut -d'/' -f2 |sort -u)
 
-# If both .toml and .py file of a flow has changed those immediate parent directory entries will be add twice so removing the duplicates
-duplicatesRemoved=$(echo "$list" | awk '{for (i=1;i<=NF;i++) if (!a[$i]++) printf("%s%s",$i,FS)}{printf("\n")}')
-
-echo $duplicatesRemoved # Priting for debugging purpose
-
-# Loop will parse all files and append it with 'FLOW_NAME=' to pass this parameter in downstream job
-files=($duplicatesRemoved)
-for file in "${files[@]}"
+# Loop will parse list and append it with 'FLOW_NAME=' to pass this parameter in downstream job
+list=($FLOWS_TO_DEPLOY)
+for item in "${list[@]}"
 do
   # Writing all the changed flows in downstream.properties file which is passed as a parameter to downstream job
-  echo -n "prefect-flows-deployment-${file}, " >> "${WORKSPACE}/downstream.properties"
+  echo -n "prefect-flows-deployment-${item}, " >> "${WORKSPACE}/downstream.properties"
 done
 
 sed -i 's/[ \t]*$//' ${WORKSPACE}/downstream.properties # Removing trailing space character
