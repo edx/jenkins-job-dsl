77b46d718152dbcc8b7ad00e6f193d9d8cec9146
Arbab Khalil <akhalil@edx.org>
2021-08-17 15:50:04 +0500

    PSRE-974 | Create Data Czar self service (#1409)
    Adding Create Data Czar self service job.

---
+++ devops/jobs/CreateDataCzar.groovy
@@ -0,0 +1,105 @@
+/*
+ Create Data Czar
+
+ Variables without defaults are marked (required) 
+ 
+ Variables consumed for this job:
+    * SECURE_GIT_CREDENTIALS: secure-bot-user (required)
+    * NOTIFY_ON_FAILURE: alert@example.com
+    * FOLDER_NAME: folder
+    * SYS_ADMIN_REPO: repo where the mongo backup script is located (required)
+    * STATUS_BRANCH: default is master
+    * ORGANIZATION: organization name to create data czar for
+    * GPG_KEY: Key required for encryptring aws credentials
+    This job expects the following credentials to be defined on the folder
+    tools-edx-jenkins-aws-credentials: file with key/secret in boto config format
+    
+
+*/
+package devops.jobs
+import static org.edx.jenkins.dsl.Constants.common_logrotator
+import static org.edx.jenkins.dsl.Constants.common_wrappers
+
+class CreateDataCzar{
+    public static def job = { dslFactory, extraVars ->
+        dslFactory.job(extraVars.get("FOLDER_NAME","Data Czar") + "/create-data-czar") {
+
+            logRotator common_logrotator
+            wrappers common_wrappers
+
+            wrappers{
+                credentialsBinding{
+                    file('AWS_CONFIG_FILE','tools-edx-jenkins-aws-credentials')
+                    string('ROLE_ARN', "create-data-czar-edx-role-arn")
+                }
+            }
+
+            parameters{
+                stringParam('CONFIGURATION_REPO', extraVars.get('CONFIGURATION_REPO', 'https://github.com/edx/configuration.git'),
+                            'Git repo containing edX configuration.')
+                stringParam('CONFIGURATION_BRANCH', extraVars.get('CONFIGURATION_BRANCH', 'master'),
+                        'e.g. tagname or origin/branchname')
+                stringParam('ORGANIZATION',
+                        'Name of organization to create data czar. e.g alaskax')
+                booleanParam('CREATE_ORG', 'true',
+                        'true if create new organization or false if organization is already created.')
+                stringParam('USER_EMAIL',
+                        'User Email address to generate Data Czar')
+                fileParam('user_gpg_key.gpg', 
+                        'Paste the GPG key for encryption of aws credentials')
+            }
+
+            multiscm{
+                git {
+                    remote {
+                        url('$CONFIGURATION_REPO')
+                        branch('$CONFIGURATION_BRANCH')
+                    }
+                    extensions {
+                        cleanAfterCheckout()
+                        pruneBranches()
+                        relativeTargetDirectory('configuration')
+                    }
+                }
+            }
+
+
+            steps {
+               shell(dslFactory.readFileFromWorkspace('devops/resources/create-data-czar.sh'))
+
+            }
+
+            publishers {
+                archiveArtifacts {
+                    pattern('sysadmin/create_data_czar/' + ${USER_EMAIL} + '-credentials.txt.gpg')
+                    onlyIfSuccessful()
+                }
+
+            if (extraVars.get('NOTIFY_ON_FAILURE')){
+                publishers {
+                    archiveArtifacts {
+                        pattern('build/test-output/**/*.html')
+                        onlyIfSuccessful()
+                    }
+                    extendedEmail {
+                        recipientList(extraVars.get('NOTIFY_ON_FAILURE'))
+                        triggers {
+                                failure {
+                                    attachBuildLog(false)
+                                    compressBuildLog(false)
+                                    subject('Failed build: ${JOB_NAME} #${BUILD_NUMBER}')
+                                    content('Jenkins job: ${JOB_NAME} failed. \nFor' + " ${ORGANIZATION} " + 'Organization. \nFor' + " ${USER_EMAIL} " + 'User Email. \n\nSee ${BUILD_URL} for details.')
+                                    contentType('text/plain')
+                                    sendTo {
+                                        recipientList()
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+}
---
+++ devops/resources/create-data-czar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -exuo pipefail
+
+set +u
+. /edx/var/jenkins/jobvenvs/virtualenv_tools.sh
+# creates a venv with its location stored in variable "venvpath"
+create_virtualenv --python=python3.8 --clear
+. "$venvpath/bin/activate"
+set -u
+
+cd $WORKSPACE/configuration
+pip install -r requirements.txt
+. util/jenkins/assume-role.sh
+
+assume-role ${ROLE_ARN}
+
+cd util/create_data_czar
+
+# Create Policy
+if CREATE_ORG == "true"  then;
+    python ./create_org_data_czar_policy.py --org ${ORGANIZATION}
+    python ./create_data_czar.py --user ${USER_EMAIL} --file $WORKSPACE/user_gpg_key.gpg --org ${ORGANIZATION}
+else
+    # Create User and add to group
+    python ./create_data_czar.py --user ${USER_EMAIL} --file $WORKSPACE/user_gpg_key.gpg --org ${ORGANIZATION}
