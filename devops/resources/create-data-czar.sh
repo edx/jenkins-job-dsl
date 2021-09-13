ff468094ebc4379f84ba5207736e0d894e37e7cc
Arbab Khalil <akhalil@edx.org>
2021-09-13 19:15:50 +0500

    fix: Remove unused creator argument from data czar script | PSRE-1038 (#1428)

--- devops/resources/create-data-czar.sh
+++ devops/resources/create-data-czar.sh
@@ -18,7 +18,7 @@ cd util/create_data_czar
 
 # Create Policy
 if [ ${CREATE_ORG} == "true"  ]; then
-    python ./create_org_data_czar_policy.py --org ${ORGANIZATION} --creator ${BUILD_USER_ID}
+    python ./create_org_data_czar_policy.py --org ${ORGANIZATION}
     python ./create_data_czar.py --user ${USER_EMAIL} --file "$WORKSPACE/user_gpg_key.gpg" --org ${ORGANIZATION} --creator ${BUILD_USER_ID}
 else
     # Create User and add to group
