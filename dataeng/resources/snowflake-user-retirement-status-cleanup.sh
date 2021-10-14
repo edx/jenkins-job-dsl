32b0e4b7894fc9f9dcd91638a1c4094e0f3a6034
Stu Young <estute@users.noreply.github.com>
2021-10-14 16:09:42 -0400

    feat: add new job to run user retirement cleanup script against snowflake (#1445)

---
+++ dataeng/resources/snowflake-user-retirement-status-cleanup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -ex
+
+# Setup
+cd $WORKSPACE/analytics-tools/snowflake
+make requirements
+
+python retirement_cleanup.py \
+    --key_path $WORKSPACE/analytics-secure/$KEY_PATH \
+    --passphrase_path $WORKSPACE/analytics-secure/$PASSPHRASE_PATH \
+    --user $USER \
+    --account $ACCOUNT
