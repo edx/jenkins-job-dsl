00bdac7d5da8d8319cbeb3fbd3be8821c9919b66
Jeremy Bowman <jbowman@edx.org>
2021-11-16 09:28:21 -0500

    build: Remove remaining "make upgrade" jobs (#1464)
    All of the remaining "make upgrade" jobs were for repositories which are either
    archived or already running this task in GitHub Actions also.  Removed these
    jobs and the view intended to show just them.  Also removed a view for
    edx-platform Django upgrade test jobs, as there are currently none and any
    future ones will run in GitHub Actions instead.

--- platform/views/platformViews.groovy
+++ platform/views/platformViews.groovy
@@ -53,12 +53,3 @@ branchList.each { branch ->
     }
 
 }
-
-listView('django-upgrade-pr-tests') {
-    description('jobs used to run tests on pull requests against various ' +
-                'versions of django during the upgrade process')
-    jobs {
-        regex('edx-platform-django-.*-pr')
-    }
-    columns DEFAULT_VIEW.call()
-}
