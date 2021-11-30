256e484ee9f6868f60b05bd32442c4b1b9e89a1b
Abdoul Aw <aaw@edx.org>
2021-11-30 11:42:06 -0500

    feat: move spigot notifications to slack channel sre-notifications

--- testeng/jobs/toggleSpigot.groovy
+++ testeng/jobs/toggleSpigot.groovy
@@ -123,7 +123,7 @@ secretMap.each { jobConfigs ->
                     includeFailedTests false
                     includeCustomMessage true
                     customMessage '@here The Spigot is now: $SPIGOT_STATE ($SPIGOT_MESSAGE)'
-                    room 'TestEngineering'
+                    room 'sre-notifications'
                     matrixTriggerMode ONLY_CONFIGURATIONS
                     commitInfoChoice NONE
                 }
