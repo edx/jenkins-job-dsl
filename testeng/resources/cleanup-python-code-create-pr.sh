bf5ecb09d8b09f93b98a520b05fb6d95a8cfe267
Jawayria <jawayriahashmi@gmail.com>
2022-02-03 19:18:48 +0500

    fix: nvm not found

--- testeng/resources/cleanup-python-code-create-pr.sh
+++ testeng/resources/cleanup-python-code-create-pr.sh
@@ -6,7 +6,9 @@ set -eu -o pipefail
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.bashrc
 nvm list-remote
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 jenkins ALL= NOPASSWD: ALL
 
