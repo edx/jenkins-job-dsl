8dba342c14455f5de73527f72e2559a230bc06ff
Syed Awais Ali <awaisalisabir@gmail.com>
2022-03-16 12:16:18 +0500

    fix: mongo automation agent script

--- devops/resources/mongo-agents-update-check.sh
+++ devops/resources/mongo-agents-update-check.sh
@@ -13,21 +13,16 @@ cd $WORKSPACE/configuration
 
 base_url=$(grep 'base_url' playbooks/roles/mongo_mms/defaults/main.yml | cut -d':' -f2- | tr -d '"')
 
-curl -OL $base_url/monitoring/mongodb-mms-monitoring-agent_latest_amd64.ubuntu1604.deb
-curl -OL $base_url/backup/mongodb-mms-backup-agent_latest_amd64.ubuntu1604.deb
+curl -OL $base_url/automation/mongodb-mms-automation-agent-manager-latest.amd64.ubuntu1604.deb
 
-available_monitoring_agent_version=$(dpkg-deb -f mongodb-mms-monitoring-agent_latest_amd64.ubuntu1604.deb Version)
-available_backup_agent_version=$(dpkg-deb -f mongodb-mms-backup-agent_latest_amd64.ubuntu1604.deb Version)
-AVAILABLE_MONGO_AGENTS="$available_monitoring_agent_version $available_backup_agent_version"
+available_mongo_automation_agent_version=$(dpkg-deb -f mongodb-mms-automation-agent-manager-latest.amd64.ubuntu1604.deb Version)
 
-installed_monitoring_agent=$(grep 'version' playbooks/roles/mongo_mms/defaults/main.yml | awk -F ":" 'NR == 2 {print $2}' | tr -d '" ')
-installed_backup_agent=$(grep 'version' playbooks/roles/mongo_mms/defaults/main.yml | awk -F ":" 'NR == 3 {print $2}' | tr -d '" ')
-INSTALLED_MONGO_AGENTS="$installed_monitoring_agent $installed_backup_agent"
+AVAILABLE_MONGO_AGENTS="$available_mongo_automation_agent_version"
 
-for agent in $AVAILABLE_MONGO_AGENTS;do
-    echo ${INSTALLED_MONGO_AGENTS} | grep --quiet  "${agent}"
-    if [[ ! $? -eq 0 ]];then
-      echo "Mongo Agent Update is Available: ${agent}"
-      exit 1
-    fi
-done
+installed_mongo_automation_agent=$(grep 'version' playbooks/roles/mongo_mms/defaults/main.yml | awk -F ":" 'NR == 2 {print $2}' | tr -d '" ')
+INSTALLED_MONGO_AGENTS="$installed_mongo_automation_agent"
+
+if [[ "$AVAILABLE_MONGO_AGENTS" != "$INSTALLED_MONGO_AGENTS" ]];then
+    echo "Mongo Automation Agent Update is Available, You need to upgrade to $AVAILABLE_MONGO_AGENTS version by replacing it in https://github.com/openedx/configuration/blob/master/playbooks/roles/mongo_mms/defaults/main.yml#L11"
+    exit 1
+fi
