9b7eb143f2c18c965562495eeaa705402bb7c1c1
syedimranhassan <imran.hassan@arbisoft.com>
2022-04-20 22:16:20 +0500

    fix: Update python virtualenv to 3.8 for mgmt cmds jobs

--- devops/resources/run-ansible.sh
+++ devops/resources/run-ansible.sh
@@ -6,7 +6,7 @@ cd $WORKSPACE/configuration
 
 . /edx/var/jenkins/jobvenvs/virtualenv_tools.sh
 # creates a venv with its location stored in variable "venvpath"
-create_virtualenv --python=python3.6 --clear
+create_virtualenv --python=python3.8 --clear
 . "$venvpath/bin/activate"
 
 pip install -r requirements.txt
