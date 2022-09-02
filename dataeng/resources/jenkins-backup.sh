f0b1dd558f2a9ea60d839c48aca261060accd7d7
MuhammadZakaria <muhammadzakaria@A006-00917.local>
2022-09-02 19:31:07 +0500

    chore: Update Jenkins Backup name

--- dataeng/resources/jenkins-backup.sh
+++ dataeng/resources/jenkins-backup.sh
@@ -14,9 +14,15 @@ cp $JENKINS_HOME/*.xml jenkins/
 #cp -r $JENKINS_HOME/users jenkins/
 # Copy job definitions into the workspace
 rsync -am --include='config.xml' --include='*/' --prune-empty-dirs --exclude='*' $JENKINS_HOME/jobs/ jenkins/jobs/
+# Get current UTC date and time with minutes 
+CURRENT_UTC_TIME=$(TZ=UTC date -I minutes)
+# jenkins backup name
+BACKUP_NAME=${NODE_NAME}-build${BUILD_ID}-${CURRENT_UTC_TIME}
 # Create an archive from all copied files (since the S3 plugin cannot copy folders recursively)
-tar czf $BUILD_ID.tar.gz jenkins/
+tar czf $BACKUP_NAME.tar.gz jenkins/
 # Remove the directory so only the archive gets copied to S3
 rm -rf jenkins
 
-aws s3 cp ${BUILD_ID}.tar.gz $S3_BACKUP_BUCKET/jenkins-analytics/${BUILD_ID}.tar.gz
+aws s3 cp ${BACKUP_NAME}.tar.gz $S3_BACKUP_BUCKET/jenkins-analytics/${BACKUP_NAME}.tar.gz
+# Remove tar jenkins backup file after uploading to S3 
+rm -rf ${BACKUP_NAME}.tar.gz
