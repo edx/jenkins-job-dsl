15255b96356da6fa1dbc35dff632afc4aa2edb34
Purna Shah <120611704+purna2U@users.noreply.github.com>
2023-04-11 12:24:07 -0700

    chore: decided to try removing common_publishers

--- dataeng/jobs/analytics/TestHelloWorld.groovy
+++ dataeng/jobs/analytics/TestHelloWorld.groovy
@@ -6,15 +6,11 @@ import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
 class TestHelloWorld {
     public static def job = { dslFactory, allVars ->
         dslFactory.job('test-hello-world') {
-            parameters {
-                stringParam('NOTIFY', allVars.get('NOTIFY','$PAGER_NOTIFY'), 'Space separated list of emails to send notifications to.')
-            }
             logRotator common_log_rotator(allVars)
             triggers common_triggers(allVars)
             wrappers {
                 timestamps()
             }
-            publishers common_publishers(allVars)
             steps {
                 shell(dslFactory.readFileFromWorkspace('dataeng/resources/test-hello-world.sh'))
             }
