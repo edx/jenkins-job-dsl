55da35ebec23fad3257f02480cc4706c092e371a
Hassan <hassan@edx.org>
2023-07-27 11:03:58 -0500

    Update PrefectFlowsDeployment.groovy

--- dataeng/jobs/analytics/PrefectFlowsDeployment.groovy
+++ dataeng/jobs/analytics/PrefectFlowsDeployment.groovy
@@ -116,6 +116,7 @@ class PrefectFlowsDeployment{
             'load_segment_config_to_snowflake',
             'send_transactions_to_vertex',
             'send_refunds_to_vertex',
+            'send_transactions_refunds_to_vertex',
         ]
         prefect_flows.each { prefect_flow ->
             dslFactory.job("prefect-flows-deployment-$prefect_flow"){
