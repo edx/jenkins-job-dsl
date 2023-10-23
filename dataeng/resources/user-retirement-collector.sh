55fecd404c1a687d175f6d15857eb66699c0910b
syedimranhassan <imran.hassan@arbisoft.com>
2023-10-23 15:12:24 +0500

    chore: Remove secure repo and fetch secrets from AWS for analytics jenkins jobs

--- dataeng/resources/user-retirement-collector.sh
+++ dataeng/resources/user-retirement-collector.sh
@@ -21,6 +21,21 @@ source "${VENV}/bin/activate"
 export PYTHONIOENCODING=UTF-8
 export LC_CTYPE=en_US.UTF-8
 
+#Fetch secrets from AWS
+cd $WORKSPACE/configuration
+pip install -r util/jenkins/requirements.txt
+# hide the sensitive information in the logs
+set +x
+
+CONFIG_YAML=$(aws secretsmanager get-secret-value --secret-id "user-retirement-secure/$ENVIRONMENT" --region "us-east-1" --output json | jq -r '.SecretString' | yq -y .)
+
+# Create a temporary file to store the YAML
+TEMP_CONFIG_YAML=$(mktemp $WORKSPACE/tempfile.XXXXXXXXXX.yml)
+
+# Write the YAML data to the temporary file
+echo "$CONFIG_YAML" > "$TEMP_CONFIG_YAML"
+
+set -x
 
 # prepare tubular
 cd $WORKSPACE/tubular
@@ -35,8 +50,11 @@ mkdir $LEARNERS_TO_RETIRE_PROPERTIES_DIR
 
 # Call the script to collect the list of learners that are to be retired.
 python scripts/get_learners_to_retire.py \
-    --config_file=$WORKSPACE/user-retirement-secure/$ENVIRONMENT.yml \
+    --config_file=$TEMP_CONFIG_YAML \
     --output_dir=$LEARNERS_TO_RETIRE_PROPERTIES_DIR \
     --cool_off_days=$COOL_OFF_DAYS \
     --user_count_error_threshold=$USER_COUNT_ERROR_THRESHOLD \
     --max_user_batch_size=$MAX_USER_BATCH_SIZE
+
+# Remove the temporary file after processing
+rm -f "$TEMP_CONFIG_YAML"
--- dataeng/resources/user-retirement-driver.sh
+++ dataeng/resources/user-retirement-driver.sh
@@ -21,6 +21,22 @@ source "${VENV}/bin/activate"
 export PYTHONIOENCODING=UTF-8
 export LC_CTYPE=en_US.UTF-8
 
+#Fetch secrets from AWS
+cd $WORKSPACE/configuration
+pip install -r util/jenkins/requirements.txt
+# hide the sensitive information in the logs
+set +x
+
+CONFIG_YAML=$(aws secretsmanager get-secret-value --secret-id "user-retirement-secure/$ENVIRONMENT" --region "us-east-1" --output json | jq -r '.SecretString' | yq -y .)
+
+# Create a temporary file to store the YAML
+TEMP_CONFIG_YAML=$(mktemp $WORKSPACE/tempfile.XXXXXXXXXX.yml)
+
+# Write the YAML data to the temporary file
+echo "$CONFIG_YAML" > "$TEMP_CONFIG_YAML"
+
+set -x
+
 # prepare tubular
 cd $WORKSPACE/tubular
 # snapshot the current latest versions of pip and setuptools.
@@ -30,4 +46,7 @@ pip install -r requirements.txt
 # Call the script to retire one learner.  This assumes the following build
 # parameters / environment variable is set: RETIREMENT_USERNAME.
 python scripts/retire_one_learner.py \
-    --config_file=$WORKSPACE/user-retirement-secure/$ENVIRONMENT.yml
+    --config_file=$TEMP_CONFIG_YAML
+
+# Remove the temporary file after processing
+rm -f "$TEMP_CONFIG_YAML"
