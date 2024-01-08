ac0dd1c0f7a65cc177ce1b75fa7aab24162a8b6f
nadeemshahzad <mshahzad@2u.com>
2024-01-08 13:59:18 +0500

    chore: remove sysadmin repo. reference

--- devops/jobs/SAMLSSLExpirationCheck.groovy
+++ devops/jobs/SAMLSSLExpirationCheck.groovy
@@ -9,8 +9,8 @@
                  saml_secret (required)
                  secret_key (required)
     * SECURE_GIT_CREDENTIALS: secure-bot-user (required)
-    * CONFIGURATION_REPO: name of config repo, default is https://github.com/edx/configuration.git
-    * CONFIGURATION_BRANCH: default is master
+    * MONITORING_SCRIPT_REPO: name of config repo, default is https://github.com/edx/configuration.git
+    * MONITORING_SCRIPT_REPO_BRANCH: default is master
     * REGION: default is us-east-1
     * NOTIFY_ON_FAILURE: alert@example.com
     * FOLDER_NAME: folder, default is Monitoring
@@ -21,40 +21,69 @@
 package devops.jobs
 import static org.edx.jenkins.dsl.Constants.common_logrotator
 
-class SAMLSSLExpirationCheck{
-    public static def job = { dslFactory, extraVars ->
-        assert extraVars.containsKey('DEPLOYMENTS') : "Please define DEPLOYMENTS. It should be a list of strings."
-        assert !(extraVars.get('DEPLOYMENTS') instanceof String) : "Make sure DEPLOYMENTS is a list and not a string"
-        extraVars.get('DEPLOYMENTS').each { deployment, configuration ->
-                configuration.environments.each { environment, inner_config -> 
-                    dslFactory.job(extraVars.get("FOLDER_NAME","Monitoring") + "/saml-ssl-expiration-check-${environment}-${deployment}") {
-                        logRotator common_logrotator
+class SAMLSSLExpirationCheck {
+  public static def job = {
+    dslFactory,
+    extraVars ->
+    assert extraVars.containsKey('DEPLOYMENTS'): "Please define DEPLOYMENTS. It should be a list of strings."
+    assert!(extraVars.get('DEPLOYMENTS') instanceof String): "Make sure DEPLOYMENTS is a list and not a string"
+    extraVars.get('DEPLOYMENTS').each {
+      deployment,
+      configuration ->
+      configuration.environments.each {
+        environment,
+        inner_config ->
+        dslFactory.job(extraVars.get("FOLDER_NAME", "Monitoring") + "/saml-ssl-expiration-check-${environment}-${deployment}") {
+          logRotator common_logrotator
 
+          def gitCredentialId = extraVars.get('SECURE_GIT_CREDENTIALS', '')
 
-                        def gitCredentialId = extraVars.get('SECURE_GIT_CREDENTIALS','')
+          parameters {
+            stringParam('MONITORING_SCRIPTS_REPO', extraVars.get('MONITORING_SCRIPTS_REPO', 'git@github.com:edx/monitoring-scripts.git'),
+              'Git repo containing edX monitoring scripts, which contains the ssl expiration check script.')
+            stringParam('MONITORING_SCRIPTS_BRANCH', extraVars.get('MONITORING_SCRIPTS_BRANCH', 'master'),
+              'e.g. tagname or origin/branchname')
+          }
 
-                        triggers {
-                            cron("H 15 * * * ")
-                        }
+          multiscm {}
+          git {
+            remote {
+              url('$MONITORING_SCRIPTS_REPO')
+              branch('$MONITORING_SCRIPTS_BRANCH')
+              if (gitCredentialId) {
+                credentials(gitCredentialId)
+              }
+            }
+            extensions {
+              cleanAfterCheckout()
+              pruneBranches()
+              relativeTargetDirectory('monitoring-scripts')
+            }
+          }
+        }
 
-                        environmentVariables {
-                            env('REGION', extraVars.get('REGION','us-east-1'))
-                            env('DAYS', extraVars.get('DAYS', 90))
-                            env('SAML_SECRET',inner_config.get('saml_secret'))
-                            env('SECRET_KEY',inner_config.get('secret_key'))
-                        }
+        triggers {
+          cron("H 15 * * * ")
+        }
 
-                        steps {
-                          shell(dslFactory.readFileFromWorkspace('devops/resources/saml-ssl-expiration-check.sh'))
-                       }
+        environmentVariables {
+          env('REGION', extraVars.get('REGION', 'us-east-1'))
+          env('DAYS', extraVars.get('DAYS', 90))
+          env('SAML_SECRET', inner_config.get('saml_secret'))
+          env('SECRET_KEY', inner_config.get('secret_key'))
+        }
 
-                       if (extraVars.get('NOTIFY_ON_FAILURE')){
-                           publishers {
-                               mailer(extraVars.get('NOTIFY_ON_FAILURE'), false, false)
-                       }
-                  }
-               }
-           }
-       }
-   }
+        steps {
+          shell(dslFactory.readFileFromWorkspace('devops/resources/saml-ssl-expiration-check.sh'))
+        }
+
+        if (extraVars.get('NOTIFY_ON_FAILURE')) {
+          publishers {
+            mailer(extraVars.get('NOTIFY_ON_FAILURE'), false, false)
+          }
+        }
+      }
+    }
+  }
+}
 }
