cef597d09631b4c98f12323a18460f32c837d1ec
Hassan <hassan.javeed84@gmail.com>
2024-03-13 16:33:40 -0500

    chore: Added missing import.

--- dataeng/jobs/analytics/RetirementJobs.groovy
+++ dataeng/jobs/analytics/RetirementJobs.groovy
@@ -6,6 +6,7 @@ import static org.edx.jenkins.dsl.AnalyticsConstants.secure_scm
 import static org.edx.jenkins.dsl.AnalyticsConstants.secure_scm_parameters
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_wrappers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_authorization
+import static org.edx.jenkins.dsl.AnalyticsConstants.platform_scm
 
 class RetirementJobs{
     public static def job = { dslFactory, allVars ->
