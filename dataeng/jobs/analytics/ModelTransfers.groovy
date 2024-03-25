0da7e1fadfba1a7d4a26d54635fc849ac37e4915
katebygrace <kate.ruh@gmail.com>
2024-03-25 15:22:52 -0700

    chore: model transfers

--- dataeng/jobs/analytics/ModelTransfers.groovy
+++ dataeng/jobs/analytics/ModelTransfers.groovy
@@ -44,6 +44,7 @@ class ModelTransfers{
                 }
                 publishers common_publishers(allVars)
                 steps {
+                    shell(dslFactory.readFileFromWorkspace('dataeng/resources/secrets-manager-setup.sh'))
                     shell(dslFactory.readFileFromWorkspace('dataeng/resources/model-transfers.sh'))
                 }
             }
