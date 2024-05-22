5b49fe8d96b9e110444790bf33e734f40a594723
katebygrace <kate.ruh@gmail.com>
2024-05-22 18:36:56 -0400

    chore: stitch lag monitor secretsmanager
    JIRA:CLOUDSEC-12

--- dataeng/jobs/analytics/StitchSnowflakeLagMonitor.groovy
+++ dataeng/jobs/analytics/StitchSnowflakeLagMonitor.groovy
@@ -6,8 +6,6 @@ import static org.edx.jenkins.dsl.AnalyticsConstants.common_log_rotator
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_wrappers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_publishers
 import static org.edx.jenkins.dsl.AnalyticsConstants.common_triggers
-import static org.edx.jenkins.dsl.AnalyticsConstants.secure_scm_parameters
-import static org.edx.jenkins.dsl.AnalyticsConstants.secure_scm
 
 
 
@@ -15,7 +13,6 @@ class StitchSnowflakeLagMonitor {
     public static def job = { dslFactory, allVars ->
         dslFactory.job("stitch-snowflake-lag-monitor") {
             logRotator common_log_rotator(allVars)
-            parameters secure_scm_parameters(allVars)
             parameters {
                 stringParam('ANALYTICS_TOOLS_URL', allVars.get('ANALYTICS_TOOLS_URL'), 'URL for the analytics tools repo.')
                 stringParam('ANALYTICS_TOOLS_BRANCH', allVars.get('ANALYTICS_TOOLS_BRANCH'), 'Branch of analtyics tools repo to use.')
@@ -29,7 +26,7 @@ class StitchSnowflakeLagMonitor {
                 env('ACCOUNT', allVars.get('ACCOUNT'))
             }
             logRotator common_log_rotator(allVars)
-            multiscm secure_scm(allVars) << {
+            multiscm {
                 git {
                     remote {
                         url('$ANALYTICS_TOOLS_URL')
