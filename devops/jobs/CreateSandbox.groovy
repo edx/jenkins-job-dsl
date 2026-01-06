877bfe3af27194e8b144b7240b33536bdf3018da
Vaibhav Gulati <vgulati-apphelix@2U.com>
2026-01-06 12:41:10 +0530

    fix: sandbox build failure (#1822)
    Changes included
    edxapp_version: Changed from master to release-ulmo
    edx_platform_repo: Changed from https://github.com/openedx/edx-platform.git to https://github.com/edx/edx-platform.git
    Added documentation: Included comprehensive comment explaining the change and future maintenance considerations

--- devops/jobs/CreateSandbox.groovy
+++ devops/jobs/CreateSandbox.groovy
@@ -136,9 +136,18 @@ class CreateSandbox {
                 booleanParam("edx_demo_course",true,"")
 
                 booleanParam("edxapp",true,"")
-                stringParam("edxapp_version","master","")
-                stringParam("edx_platform_repo","https://github.com/openedx/edx-platform.git",
-                            "If building a sandbox to test an external configuration PR, replace this with the fork of configuration.git's https URL")
+                /*
+                    https://2u-internal.atlassian.net/browse/BOMS-356 sandbox builds are broken
+                    as the default sandbox build uses openedx/edx-platform master.
+                    Sandbox build should now use edx/edx-platform and default branch release-ulmo.
+                    The edx-platform repo has moved under the edx org, and release-ulmo is
+                    currently the default branch for builds. After the next release, the default
+                    branch will change and this value will need to be updated again for sandbox
+                    creation.
+                */
+                stringParam("edxapp_version","release-ulmo","")
+                stringParam("edx_platform_repo","https://github.com/edx/edx-platform.git",
+                            "If building a sandbox to test edx-platform PR, replace this with the fork of edx-platform.git's https URL")
                 booleanParam("edxapp_container_enabled",false,"Checking this option will setup edx-platform in a Docker containers rather than being setup via Ansible.")
                 booleanParam("edxapp_decrypt_and_copy_config_enabled",true,"Checking this option will decrypt and copy edxapp config file from configuration internal repo.")
 
