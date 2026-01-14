d94bc3cb31f56b193f33b758a3d369d60e25dfd3
ktyagiapphelix2u <ktyagi-apphelix@2u.com>
2026-01-14 11:31:37 +0000

    fix: redacting user retirement data in lms

--- devops/resources/user-retirement-archiver.sh
+++ devops/resources/user-retirement-archiver.sh
@@ -46,6 +46,7 @@ python scripts/user_retirement/retirement_archive_and_cleanup.py \
     --batch_size=$BATCH_SIZE \
     --start_date=$START_DATE \
     --end_date=$END_DATE \
+    --jenkins_run_id=$BUILD_NUMBER \
     --dry_run=$DRY_RUN
 
 # Remove the temporary file after processing
